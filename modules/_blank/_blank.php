3565fc28c556816255b77b4274460501832bc45e
MeanEYE.rcf <unknown>
2010-06-02 15:35:38 +0200

    Added _blank module

---
+++ modules/_blank/_blank.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * BLANK MODULE
+ *
+ * @author MeanEYE
+ * @copyright RCF Group,2008.
+ */
+
+class _blank extends Module {
+
+	/**
+	 * Constructor
+	 *
+	 * @return journal
+	 */
+	function _blank() {
+		$this->file = __FILE__;
+		parent::Module();
+	}
+
+	/**
+	 * Transfers control to module functions
+	 *
+	 * @param string $action
+	 * @param integer $level
+	 */
+	function transferControl($level, $params = array(), $children = array()) {
+		// global control actions
+		switch ($params['action']) {
+			default:
+				break;
+		}
+
+		// global control actions
+		switch ($params['backend_action']) {
+			default:
+				break;
+		}
+	}
+
+	/**
+	 * Event triggered upon module initialization
+	 */
+	function onInit() {
+
+	}
+
+	/**
+	 * Event triggered upon module deinitialization
+	 */
+	function onDeInit() {
+	}
+
+	/**
+	 * Event called upon module registration
+	 */
+	function onRegister() {
+		global $ModuleHandler;
+
+		// load module style and scripts
+		if ($ModuleHandler->moduleExists('head_tag')) {
+			$head_tag = $ModuleHandler->getObjectFromName('head_tag');
+			//$head_tag->addTag('link', array('href'=>url_GetFromFilePath($this->path.'include/_blank.css'), 'rel'=>'stylesheet', 'type'=>'text/css'));
+			//$head_tag->addTag('script', array('src'=>url_GetFromFilePath($this->path.'include/_blank.js'), 'type'=>'text/javascript'));
+		}
+
+		// register backend
+		if ($ModuleHandler->moduleExists('backend')) {
+			$backend = $ModuleHandler->getObjectFromName('backend');
+
+			//$group = new backend_MenuGroup("Blank", "", $this->name);
+			//$group->addItem(new backend_MenuItem("Menu Item", "", "", 1));
+
+			//$backend->addMenu($group);
+		}
+	}
+}
