8513377abf87d548fbcd7ec7b571ac4cecb300af
MeanEYE.rcf <unknown>
2010-06-28 12:46:06 +0200

    Added articles module

---
+++ modules/articles/articles.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * BLANK MODULE
+ *
+ * @author MeanEYE
+ * @copyright RCF Group,2008.
+ */
+
+class articles extends Module {
+
+	/**
+	 * Constructor
+	 *
+	 * @return _blank
+	 */
+	function articles() {
+		$this->file = __FILE__;
+		parent::Module();
+	}
+
+	/**
+	 * Transfers control to module functions
+	 *
+	 * @param string $action
+	 * @param integer $level
+	 */
+	function transferControl($level, $params = array(), $children = array()) {
+		// global control actions
+		switch ($params['action']) {
+			default:
+				break;
+		}
+
+		// global control actions
+		switch ($params['backend_action']) {
+			default:
+				break;
+		}
+	}
+
+	/**
+	 * Event triggered upon module initialization
+	 */
+	function onInit() {
+
+	}
+
+	/**
+	 * Event triggered upon module deinitialization
+	 */
+	function onDisable() {
+	}
+
+	/**
+	 * Event called upon module registration
+	 */
+	function onRegister() {
+		global $ModuleHandler;
+
+		// load module style and scripts
+		if ($ModuleHandler->moduleExists('head_tag')) {
+			$head_tag = $ModuleHandler->getObjectFromName('head_tag');
+			//$head_tag->addTag('link', array('href'=>url_GetFromFilePath($this->path.'include/_blank.css'), 'rel'=>'stylesheet', 'type'=>'text/css'));
+			//$head_tag->addTag('script', array('src'=>url_GetFromFilePath($this->path.'include/_blank.js'), 'type'=>'text/javascript'));
+		}
+
+		// register backend
+		if ($ModuleHandler->moduleExists('backend')) {
+			$backend = $ModuleHandler->getObjectFromName('backend');
+		}
+	}
+}
