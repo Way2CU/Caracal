88d313d35a3ccfae848228626829859a9939bb56
MeanEYE.rcf <unknown>
2010-06-28 12:52:28 +0200

    Additional work on swfobject

--- modules/swfobject/swfobject.php
+++ modules/swfobject/swfobject.php
@@ -33,6 +33,15 @@ class swfobject extends Module {
 	function transferControl($level, $params = array(), $children = array()) {
 		// global control actions
 		switch ($params['action']) {
+			case 'embed':
+				$flash_params = array(
+									'wmode'		=> 'transparent',
+									'menu'		=> false,
+								);
+				$this->embedSWF($level, $params['url'], $params['id'],
+								$params['width'], $params['height'], array(), $flash_params);
+				break;
+
 			default:
 				break;
 		}
@@ -56,10 +65,10 @@ class swfobject extends Module {
 			$head_tag->addTag('script', array('src'=>url_GetFromFilePath($this->path.'include/swfobject.js'), 'type'=>'text/javascript'));
 		}
 	}
-	
+
 	/**
 	 * Embed flash player with specified parameters
-	 * 
+	 *
 	 * @param string $url
 	 * @param string $target_id
 	 * @param integer $width
@@ -71,16 +80,60 @@ class swfobject extends Module {
 		$template = new TemplateHandler('embed.xml', $this->path.'templates/');
 		$template->setMappedModule($this->name);
 
+		$p_flashvars = $this->getParams($flash_vars);
+		$p_params = $this->getParams($params);
+		$script = "swfobject.embedSWF('{$url}', '{$target_id}', '{$width}', '{$height}', '{$this->flash_version}', null, {$p_flashvars}, {$p_params});";
+
 		$params = array(
-					'url'		=> "'{$url}'",
-					'id'		=> "'{$target_id}'",
-					'width'		=> "'{$width}'",
-					'height'	=> "'{$height}'",
-					'version'	=> "'{$this->flash_version}'"
+					'params'		=> $p_params,
+					'flash_vars'	=> $p_flashvars,
+					'url'			=> $url,
+					'id'			=> $target_id,
+					'width'			=> $width,
+					'height'		=> $height,
+					'version'		=> $this->flash_version,
+					'script'		=> $script
 				);
-				
+
 		$template->restoreXML();
 		$template->setLocalParams($params);
-		$template->parse($level);		
+		$template->parse($level);
+	}
+
+	/**
+	 * Get
+	 * @param unknown_type $var_name
+	 * @param unknown_type $params
+	 * @return string
+	 */
+	function getParams($params) {
+		$result = "";
+
+		foreach($params as $key=>$value) {
+			switch(gettype($value)) {
+				case 'string':
+					$value = "'{$value}'";
+					break;
+
+				case 'boolean':
+					$value = $value ? 'true' : 'false';
+					break;
+
+				case 'integer':
+					$value = strval($value);
+					break;
+
+				default:
+					$value = '';
+					break;
+			}
+
+			$last = $key == end(array_keys($params));
+			$result .= "{$key}: {$value}".($last ? '' : ', ');
+		}
+
+		$result = "{".$result."}";
+
+		return $result;
 	}
 }
