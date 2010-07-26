fcc227cfb424f1954fff9b4c9546c433179cc5bd
MeanEYE.rcf <unknown>
2010-07-26 09:29:16 +0200

    Additional work on articles module.

--- modules/articles/articles.php
+++ modules/articles/articles.php
@@ -27,20 +27,50 @@ class articles extends Module {
 	 */
 	function transferControl($level, $params = array(), $children = array()) {
 		// global control actions
-		switch ($params['action']) {
-			default:
-				break;
-		}
+		if (isset($params['action']))
+			switch ($params['action']) {
+				case 'show':
+					$this->tag_Article($level, $params, $children);
+					break;
+
+				case 'show_rating_image':
+					$this->tag_ArticleRatingImage($level, $params, $children);
+					break;
+
+				default:
+					break;
+			}
 
 		// global control actions
-		switch ($params['backend_action']) {
-			case 'articles_new':
-				$this->addArticle($level);
-				break;
+		if (isset($params['backend_action']))
+			switch ($params['backend_action']) {
+				case 'articles':
+					$this->showArticles($level);
+					break;
 
-			default:
-				break;
-		}
+				case 'articles_new':
+					$this->addArticle($level);
+					break;
+
+				case 'articles_change':
+					$this->changeArticle($level);
+					break;
+
+				case 'articles_save':
+					$this->saveArticle($level);
+					break;
+
+				case 'articles_delete':
+					$this->deleteArticle($level);
+					break;
+
+				case 'articles_delete_commit':
+					$this->deleteArticle_Commit($level);
+					break;
+
+				default:
+					break;
+			}
 	}
 
 	/**
@@ -52,25 +82,17 @@ class articles extends Module {
 		$sql = "
 			CREATE TABLE `articles` (
 				`id` INT NOT NULL AUTO_INCREMENT ,
+				`text_id` VARCHAR (32) NULL ,
 				`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-				`title` VARCHAR( 255 ) NOT NULL ,
+				`title` VARCHAR( 255 ) NOT NULL DEFAULT '',
 				`content` TEXT NOT NULL ,
 				`author` INT NOT NULL ,
 				`visible` BOOLEAN NOT NULL DEFAULT '0',
-				PRIMARY KEY ( `id` ),
-				INDEX ( `author` )
-			) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=0;";
-
-		if ($db_active == 1) $db->query($sql);
-
-		$sql = "
-			CREATE TABLE `article_rating` (
-				`id` INT NOT NULL AUTO_INCREMENT ,
-				`article` INT NOT NULL ,
+				`views` INT NOT NULL DEFAULT '0',
 				`votes_up` INT NOT NULL DEFAULT '0',
 				`votes_down` INT NOT NULL DEFAULT '0',
-				PRIMARY KEY ( `id` ) ,
-				INDEX ( `article` )
+				PRIMARY KEY ( `id` ),
+				INDEX ( `author` )
 			) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=0;";
 
 		if ($db_active == 1) $db->query($sql);
@@ -82,7 +104,7 @@ class articles extends Module {
 	function onDisable() {
 		global $db, $db_active;
 
-		$sql = "DROP TABLE IF EXISTS `articles`, `article_rating`;";
+		$sql = "DROP TABLE IF EXISTS `articles`;";
 		if ($db_active == 1) $db->query($sql);
 	}
 
@@ -115,7 +137,7 @@ class articles extends Module {
 								url_GetFromFilePath($this->path.'images/new_article.png'),
 
 								window_Open( // on click open window
-											'links_list',
+											'articles_new',
 											730,
 											$this->getLanguageConstant('title_articles_new'),
 											true, true,
@@ -123,9 +145,47 @@ class articles extends Module {
 										),
 								$level=5
 							));
+			$articles_menu->addChild('', new backend_MenuItem(
+								$this->getLanguageConstant('menu_articles_manage'),
+								url_GetFromFilePath($this->path.'images/manage.png'),
+
+								window_Open( // on click open window
+											'articles',
+											720,
+											$this->getLanguageConstant('title_articles_manage'),
+											true, true,
+											backend_UrlMake($this->name, 'articles')
+										),
+								$level=5
+							));
 
 			$backend->addMenu($this->name, $articles_menu);
 		}
+	}
+
+	/**
+	 * Show administration form for articles
+	 * @param integer $level
+	 */
+	function showArticles($level) {
+		$template = new TemplateHandler('list.xml', $this->path.'templates/');
+		$template->setMappedModule($this->name);
+
+		$params = array(
+					'link_new'		=> backend_WindowHyperlink(
+										$this->getLanguageConstant('new'),
+										'articles_new', 730,
+										$this->getLanguageConstant('title_articles_new'),
+										true, false,
+										$this->name,
+										'articles_new'
+									),
+					);
+
+		$template->registerTagHandler('_article_list', &$this, 'tag_ArticleList');
+		$template->restoreXML();
+		$template->setLocalParams($params);
+		$template->parse($level);
 	}
 
 	/**
@@ -144,5 +204,351 @@ class articles extends Module {
 		$template->restoreXML();
 		$template->setLocalParams($params);
 		$template->parse($level);
+	}
+
+	/**
+	 * Display article for modification
+	 * @param integer $level
+	 */
+	function changeArticle($level) {
+		$id = fix_id(fix_chars($_REQUEST['id']));
+		$manager = new ArticleManager();
+
+		$item = $manager->getSingleItem($manager->getFieldNames(), array('id' => $id));
+
+		$template = new TemplateHandler('change.xml', $this->path.'templates/');
+		$template->setMappedModule($this->name);
+
+		$params = array(
+					'id'			=> $item->id,
+					'text_id'		=> $item->text_id,
+					'title'			=> unfix_chars($item->title),
+					'content'		=> unfix_chars($item->content),
+					'visible' 		=> $item->visible,
+					'form_action'	=> backend_UrlMake($this->name, 'articles_save'),
+					'cancel_action'	=> window_Close('articles_change')
+				);
+
+		$template->restoreXML();
+		$template->setLocalParams($params);
+		$template->parse($level);
+	}
+
+	/**
+	 * Save article data
+	 * @param integer $level
+	 */
+	function saveArticle($level) {
+		$manager = new ArticleManager();
+
+		$id = isset($_REQUEST['id']) ? fix_id($_REQUEST['id']) : null;
+		$text_id = mysql_real_escape_string(strip_tags($_REQUEST['text_id']));
+		$title = fix_chars($_REQUEST['title']);
+		$content = mysql_real_escape_string(strip_tags($_REQUEST['content']));
+		$visible = fix_id($_REQUEST['visible']);
+
+		if (is_null($id)) {
+			$window = 'articles_new';
+			$manager->insertData(
+								array(
+									'text_id'	=> $text_id,
+									'title'		=> $title,
+									'content'	=> $content,
+									'visible'	=> $visible,
+									'author'	=> $_SESSION['uid']
+								)
+							);
+		} else {
+			$window = 'articles_change';
+			$manager->updateData(
+								array(
+									'text_id'	=> $text_id,
+									'title'		=> $title,
+									'content'	=> $content,
+									'visible'	=> $visible,
+									'author'	=> $_SESSION['uid']
+								),
+								array(
+									'id'		=> $id
+								)
+							);
+		}
+
+		$template = new TemplateHandler('message.xml', $this->path.'templates/');
+		$template->setMappedModule($this->name);
+
+		$params = array(
+					'message'	=> $this->getLanguageConstant('message_article_saved'),
+					'button'	=> $this->getLanguageConstant('close'),
+					'action'	=> window_Close($window).";".window_ReloadContent('articles'),
+				);
+
+		$template->restoreXML();
+		$template->setLocalParams($params);
+		$template->parse($level);
+	}
+
+	/**
+	 * Print confirmation dialog before deleting article
+	 * @param integer $level
+	 */
+	function deleteArticle($level) {
+		$id = fix_id(fix_chars($_REQUEST['id']));
+		$manager = new ArticleManager();
+
+		$item = $manager->getSingleItem(array('title'), array('id' => $id));
+
+		$template = new TemplateHandler('confirmation.xml', $this->path.'templates/');
+		$template->setMappedModule($this->name);
+
+		$params = array(
+					'message'		=> $this->getLanguageConstant("message_article_delete"),
+					'name'			=> $item->title,
+					'yes_text'		=> $this->getLanguageConstant("delete"),
+					'no_text'		=> $this->getLanguageConstant("cancel"),
+					'yes_action'	=> window_LoadContent(
+											'articles_delete',
+											url_Make(
+												'transfer_control',
+												'backend_module',
+												array('module', $this->name),
+												array('backend_action', 'articles_delete_commit'),
+												array('id', $id)
+											)
+										),
+					'no_action'		=> window_Close('articles_delete')
+				);
+
+		$template->restoreXML();
+		$template->setLocalParams($params);
+		$template->parse($level);
+	}
+
+	/**
+	 * Delete article and print result message
+	 * @param integer $level
+	 */
+	function deleteArticle_Commit($level) {
+		$id = fix_id(fix_chars($_REQUEST['id']));
+		$manager = new ArticleManager();
+
+		$manager->deleteData(array('id' => $id));
+
+		$template = new TemplateHandler('message.xml', $this->path.'templates/');
+		$template->setMappedModule($this->name);
+
+		$params = array(
+					'message'	=> $this->getLanguageConstant("message_article_deleted"),
+					'button'	=> $this->getLanguageConstant("close"),
+					'action'	=> window_Close('articles_delete').";".window_ReloadContent('articles')
+				);
+
+		$template->restoreXML();
+		$template->setLocalParams($params);
+		$template->parse($level);
+	}
+
+	/**
+	 * Tag handler for printing article
+	 *
+	 * @param integer $level
+	 * @param array $params
+	 * @param array $children
+	 */
+	function tag_Article($level, $tag_params, $children) {
+		$manager = new ArticleManager();
+
+		$id = isset($tag_params['id']) ? fix_id($tag_params['id']) : null;
+		$text_id = isset($tag_params['text_id']) ? mysql_real_escape_string(strip_tags($tag_params['text_id'])) : null;
+
+		// we need at least one of IDs in order to display article
+		if (is_null($id) && is_null($text_id)) return;
+
+		if (isset($tag_params['template'])) {
+			if (isset($tag_params['local']) && $tag_params['local'] == 1)
+				$template = new TemplateHandler($tag_params['template'], $this->path.'templates/'); else
+				$template = new TemplateHandler($tag_params['template']);
+		} else {
+			$template = new TemplateHandler('article.xml', $this->path.'templates/');
+		}
+
+		$template->setMappedModule($this->name);
+		$template->registerTagHandler('_article_rating_image', &$this, 'tag_ArticleRatingImage');
+
+		if (!is_null($id))
+			$item = $manager->getSingleItem($manager->getFieldNames(), array('id' => $id)); else
+			$item = $manager->getSingleItem($manager->getFieldNames(), array('text_id' => $text_id));
+
+		$timestamp = strtotime($item->timestamp);
+		$date = date($this->getLanguageConstant('format_date_short'), $timestamp);
+		$time = date($this->getLanguageConstant('format_time_short'), $timestamp);
+
+		$params = array(
+					'id'			=> $item->id,
+					'text_id'		=> $item->text_id,
+					'timestamp'		=> $item->timestamp,
+					'date'			=> $date,
+					'time'			=> $time,
+					'title'			=> $item->title,
+					'content'		=> Markdown($item->content),
+					'author'		=> $item->author,
+					'visible'		=> $item->visible,
+					'views'			=> $item->views,
+					'votes_up'		=> $item->votes_up,
+					'votes_down' 	=> $item->votes_down,
+					'rating'		=> $this->_getArticleRating($item, 10),
+				);
+
+		$template->restoreXML();
+		$template->setLocalParams($params);
+		$template->parse($level);
+
+	}
+
+	/**
+	 * Tag handler for printing article list
+	 *
+	 * @param integer $level
+	 * @param array $params
+	 * @param array $children
+	 */
+	function tag_ArticleList($level, $tag_params, $children) {
+		$manager = new ArticleManager();
+		$admin_manager = new AdministratorManager();  // session module is pre-load required module
+
+		$only_visible = isset($tag_params['only_visible']) ? $tag_params['only_visible'] == 1 : false;
+		$conditions = array();
+
+		if ($only_visible) $conditions['visible'] = 1;
+
+		$items = $manager->getItems($manager->getFieldNames(), $conditions);
+
+		if (isset($tag_params['template'])) {
+			if (isset($tag_params['local']) && $tag_params['local'] == 1)
+				$template = new TemplateHandler($tag_params['template'], $this->path.'templates/'); else
+				$template = new TemplateHandler($tag_params['template']);
+		} else {
+			$template = new TemplateHandler('list_item.xml', $this->path.'templates/');
+		}
+
+		$template->setMappedModule($this->name);
+		$template->registerTagHandler('_article', &$this, 'tag_Article');
+		$template->registerTagHandler('_article_rating_image', &$this, 'tag_ArticleRatingImage');
+
+		// give the ability to limit number of links to display
+		if (isset($tag_params['limit']))
+			$items = array_slice($items, 0, $tag_params['limit'], true);
+
+		if (count($items) > 0)
+			foreach($items as $item) {
+				$timestamp = strtotime($item->timestamp);
+				$date = date($this->getLanguageConstant('format_date_short'), $timestamp);
+				$time = date($this->getLanguageConstant('format_time_short'), $timestamp);
+
+				$params = array(
+							'id'			=> $item->id,
+							'text_id'		=> $item->text_id,
+							'timestamp'		=> $item->timestamp,
+							'date'			=> $date,
+							'time'			=> $time,
+							'title'			=> $item->title,
+							'content'		=> Markdown($item->content),
+							'author'		=> $admin_manager->getItemValue('fullname', array('id' => $item->author)),
+							'visible'		=> $item->visible,
+							'views'			=> $item->views,
+							'votes_up'		=> $item->votes_up,
+							'votes_down' 	=> $item->votes_down,
+							'rating'		=> $this->_getArticleRating($item, 10),
+							'item_change'	=> url_MakeHyperlink(
+													$this->getLanguageConstant('change'),
+													window_Open(
+														'articles_change', 	// window id
+														730,				// width
+														$this->getLanguageConstant('title_articles_change'), // title
+														false, false,
+														url_Make(
+															'transfer_control',
+															'backend_module',
+															array('module', $this->name),
+															array('backend_action', 'articles_change'),
+															array('id', $item->id)
+														)
+													)
+												),
+							'item_delete'	=> url_MakeHyperlink(
+													$this->getLanguageConstant('delete'),
+													window_Open(
+														'articles_delete', 	// window id
+														400,				// width
+														$this->getLanguageConstant('title_articles_delete'), // title
+														false, false,
+														url_Make(
+															'transfer_control',
+															'backend_module',
+															array('module', $this->name),
+															array('backend_action', 'articles_delete'),
+															array('id', $item->id)
+														)
+													)
+												),
+							'item_read'		=> '',
+						);
+
+				$template->restoreXML();
+				$template->setLocalParams($params);
+				$template->parse($level);
+			}
+	}
+
+	/**
+	 * Tag handler for printing article list
+	 *
+	 * @param integer $level
+	 * @param array $params
+	 * @param array $children
+	 */
+	function tag_ArticleRatingImage($level, $tag_params, $children) {
+	}
+
+	/**
+	 * Get article rating value based on max value specified
+	 *
+	 * @param resource $article
+	 * @param integer $max
+	 * @return integer
+	 */
+	function _getArticleRating($article, $max) {
+		$total = $article->votes_up + $article->votes_down;
+
+		if ($total == 0) {
+			// no votes recorder, return 0
+			$result = 0;
+
+		} else {
+			// we have some votes recorder
+			$current = $article->votes_up - $article->votes_down;
+			if ($current < 0) $current = 0;
+
+			$result = ($current * $max) / $total;
+		}
+
+		return $result;
+	}
+}
+
+class ArticleManager extends ItemManager {
+	function __construct() {
+		parent::ItemManager('articles');
+
+		$this->addProperty('id', 'int');
+		$this->addProperty('text_id', 'varchar');
+		$this->addProperty('timestamp', 'timestamp');
+		$this->addProperty('title', 'varchar');
+		$this->addProperty('content', 'text');
+		$this->addProperty('author', 'int');
+		$this->addProperty('visible', 'boolean');
+		$this->addProperty('views', 'int');
+		$this->addProperty('votes_up', 'int');
+		$this->addProperty('votes_down', 'int');
 	}
 }
