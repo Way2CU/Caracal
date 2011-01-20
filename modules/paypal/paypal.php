f45c91a2e3759e06939c531f8a44e9af3cbcb2da
MeanEYE.rcf <unknown>
2011-01-20 12:47:39 +0100

    Added PayPal module

---
+++ modules/paypal/paypal.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * PayPal Integration Module
+ *
+ * @author MeanEYE.rcf
+ */
+
+class paypal extends Module {
+	private static $_instance;
+
+	/**
+	 * Constructor
+	 */
+	protected function __construct() {
+		global $section;
+		
+		parent::__construct(__FILE__);
+
+		// load module style and scripts
+		if (class_exists('head_tag')) {
+			$head_tag = head_tag::getInstance();
+			//$head_tag->addTag('link', array('href'=>url_GetFromFilePath($this->path.'include/_blank.css'), 'rel'=>'stylesheet', 'type'=>'text/css'));
+			//$head_tag->addTag('script', array('src'=>url_GetFromFilePath($this->path.'include/_blank.js'), 'type'=>'text/javascript'));
+		}
+
+		// register backend
+		if (class_exists('backend')) {
+			$backend = backend::getInstance();
+		}
+	}
+
+	/**
+	 * Public function that creates a single instance
+	 */
+	public static function getInstance() {
+		if (!isset(self::$_instance))
+			self::$_instance = new self();
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Transfers control to module functions
+	 *
+	 * @param array $params
+	 * @param array $children
+	 */
+	public function transferControl($params = array(), $children = array()) {
+		// global control actions
+		if (isset($params['action']))
+			switch ($params['action']) {
+				default:
+					break;
+			}
+
+		// global control actions
+		if (isset($params['backend_action']))
+			switch ($params['backend_action']) {
+				default:
+					break;
+			}
+	}
+
+	/**
+	 * Event triggered upon module initialization
+	 */
+	public function onInit() {
+		global $db_active, $db;
+
+		$sql = "";
+
+		if ($db_active == 1) $db->query($sql);
+	}
+
+	/**
+	 * Event triggered upon module deinitialization
+	 */
+	public function onDisable() {
+		global $db_active, $db;
+
+		$sql = "";
+
+		if ($db_active == 1) $db->query($sql);
+	}
+}
+
+
+class SomeManager extends ItemManager {
+	private static $_instance;
+
+	/**
+	 * Constructor
+	 */
+	protected function __construct() {
+		parent::__construct('table_name');
+
+		$this->addProperty('id', 'int');
+	}
+
+	/**
+	 * Public function that creates a single instance
+	 */
+	public static function getInstance() {
+		if (!isset(self::$_instance))
+			self::$_instance = new self();
+
+		return self::$_instance;
+	}
+}
