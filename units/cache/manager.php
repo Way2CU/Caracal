a421fb310819f047c46a3e5908ad1a31c9839a3e
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-03-09 16:25:16 +0100

    Units: Cache: Added cache manager.

--- units/cache/manager.php
+++ units/cache/manager.php
@@ -1,17 +1,18 @@
 <?php
 
-class CacheManager {
+class CacheManager extends ItemManager {
 	private static $_instance;
 
 	/**
 	 * Constructor
 	 */
 	protected function __construct() {
-		parent::__construct('cache');
+		parent::__construct('system_cache');
 
 		$this->addProperty('uid', 'char');
 		$this->addProperty('url', 'varchar');
 		$this->addProperty('times_used', 'int');
+		$this->addProperty('times_renewed', 'int');
 		$this->addProperty('expires', 'timestamp');
 	}
 
