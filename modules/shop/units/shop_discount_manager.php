a2185b04f97c67bfd24c4f4f9f0d2c2050db5f62
MeanEYE.rcf <meaneye.rcf@gmail.com>
2012-07-23 13:48:43 +0200

    Modules: Shop: Added discount managers (still not funcitonal).

---
+++ modules/shop/units/shop_discount_manager.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Shop Discount Manager
+ *
+ * @author MeanEYE.rcf
+ */
+
+class ShopDiscountManager extends ItemManager {
+	private static $_instance;
+
+	/**
+	 * Constructor
+	 */
+	protected function __construct() {
+		parent::__construct('shop_discounts');
+
+		$this->addProperty('id', 'int');
+		$this->addProperty('type', 'int');
+		$this->addProperty('name', 'ml_varchar');
+		$this->addProperty('description', 'ml_text');
+		$this->addProperty('percent', 'decimal');
+	}
+
+	/**
+	 * Public function that creates a single instance
+	 */
+	public static function getInstance() {
+		if (!isset(self::$_instance))
+		self::$_instance = new self();
+
+		return self::$_instance;
+	}
+}
+
+class ShopDiscountItemsManager extends ItemManager {
+	private static $_instance;
+
+	/**
+	 * Constructor
+	 */
+	protected function __construct() {
+		parent::__construct('shop_discounts');
+
+		$this->addProperty('id', 'int');
+		$this->addProperty('discount', 'int');
+		$this->addProperty('item', 'int');
+	}
+
+	/**
+	 * Public function that creates a single instance
+	 */
+	public static function getInstance() {
+		if (!isset(self::$_instance))
+		self::$_instance = new self();
+
+		return self::$_instance;
+	}
+}
+
+?>
