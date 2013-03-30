ac02ed4f238f4533447385977a13efba86a54f61
MeanEYE.rcf <meaneye.rcf@gmail.com>
2013-03-30 21:36:56 +0100

    Scripts: Renamed form_steps to page_control and added some convenience functions.

--- scripts/page_control.js
+++ scripts/page_control.js
@@ -1,34 +1,33 @@
 /**
- * Support for multi-part form
+ * Page Control Implementation
  *
  * Copyright (c) 2013. by Way2CU
  * Author: Mladen Mijatov
  */
 
-function FormSteps(form_selector) {
+function PageControl(selector, page_selector) {
 	var self = this;
 
-	self.steps = null;
-	self.current_step = null;
-	self.form = null;
+	self.pages = null;
+	self.current_page = null;
+	self.container = null;
+	self.controls = null;
 
 	/**
 	 * Finalize object initalization.
 	 */
 	self.init = function() {
-		// get all steps
-		var selector = (form_selector != undefined) ? form_selector : 'form';
-		self.form = $(selector);
+		self.container = $(selector || 'div.pages');
 
-		// get all steps
-		self.steps = self.form.find('div.step');
-		self.steps.each(self._connectButtonEvents);
+		// get all pages
+		self.pages = self.container.find(page_selector || 'div.page');
+		self.pages.each(self._connectButtonEvents);
 
 		self._switchContainer(0);
 	};
 
 	/**
-	 * Connect button events for each step.
+	 * Connect button events for each page.
 	 *
 	 * @param integer index
 	 */
@@ -52,7 +51,7 @@ function FormSteps(form_selector) {
 	self._handleNext = function(event) {
 		var index = $(this).data('index');
 
-		if (index + 1 < self.steps.length) {
+		if (index + 1 < self.pages.length) {
 			self._switchContainer(index + 1)
 			event.preventDefault();
 		}
@@ -74,17 +73,17 @@ function FormSteps(form_selector) {
 	};
 
 	/**
-	 * Change active container.
+	 * Change active page.
 	 *
-	 * @param integer step
+	 * @param integer page
 	 */
-	self._switchContainer = function(step) {
-		if (self.current_step != null) {
-			var to_hide = self.steps.eq(self.current_step);
-			var to_show = self.steps.eq(step);
+	self._switchContainer = function(page) {
+		if (self.current_page != null) {
+			var to_hide = self.pages.eq(self.current_page);
+			var to_show = self.pages.eq(page);
 
 			if (to_hide != to_show)
-				// swap step containers
+				// swap pages containers
 				to_hide
 					.css('display', 'block')
 					.animate({opacity: 0}, 200, function() {
@@ -97,28 +96,53 @@ function FormSteps(form_selector) {
 							.animate({opacity: 1}, 200);
 					});
 		} else {
-			self.steps.each(function(index) {
-				$(this).css('display', index == step ? 'block' : 'none');
+			self.pages.each(function(index) {
+				$(this).css('display', index == page ? 'block' : 'none');
 			});
 		}
 
-		self.current_step = step;
+		self.current_page = page;
 	};
 
 	/**
-	 * Method that can be used to handle clicking on specified step or a button.
-	 * This method looks for HTML5 data-step parameter to determine which step to
-	 * show. If no step is defined event is ignored.
+	 * Method that can be used to handle clicking on specified page or a button.
+	 * This method looks for HTML5 data-page parameter to determine which page to
+	 * show. If no page is defined event is ignored.
 	 *
 	 * @param object event
 	 */
 	self.handleClick = function(event) {
-		var step = $(this).data('step');
+		var page = $(this).data('page');
 
-		if (step != null)
-			self._switchContainer(step);
+		if (page != null)
+			self._switchContainer(page);
 
 		event.preventDefault();
+	};
+
+	/**
+	 * Connect click event handler to children of specified selector. If children
+	 * do not specify data-page their indexes will be applied.
+	 *
+	 * @param string selector
+	 */
+	self.attachControls = function(selector) {
+		self.controls = $(selector);
+
+		if (self.controls.length > 0) {
+			// make sure every control has a page index associated
+			self.controls.each(function(index) {
+				var control = $(this);
+
+				if (control.data('page') == undefined)
+					control.data('page', index);
+			});
+
+			// conect event handler
+			self.controls.click(self.handleClick);
+		}
+
+		return self;
 	};
 
 	// finish object initialization
