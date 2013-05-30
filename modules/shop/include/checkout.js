dbca079b3ce39d64be9ee852dc45be4ff1bd8e8d
MeanEYE.rcf <meaneye.rcf@gmail.com>
2013-05-31 01:37:27 +0300

    Modules: Shop: Modified checkout form to use new features of page control.

--- modules/shop/include/checkout.js
+++ modules/shop/include/checkout.js
@@ -1,3 +1,151 @@
+/**
+ * Validate sign in page.
+ *
+ * @return boolean
+ */
+function validateSignInPage() {
+	var result = false;
+	var sign_in_form = $('div#sign_in');
+	var next_button = sign_in_form.find('button.next');
+	var backend_url = $('base').attr('href') + '/index.php';
+	var email_field = sign_in_form.find('input[name=email]');
+	var password_field = sign_in_form.find('input[name=password]');
+
+	// check which option is selected
+	var selection = sign_in_form.find('input[name=existing_user]:checked').val();
+
+	// load data from server if needed
+	if (selection == 0) {
+		var data = {
+				section: 'shop',
+				action: 'json_get_user_info',
+				email: email_field.val(),
+				password: password_field.val()
+			};
+
+		$.ajax({
+			url: backend_url,
+			type: 'GET',
+			async: false,
+			data: data,
+			dataType: 'json',
+			context: this,
+			success: function(data) {
+				result = true;
+			}, 
+			error: function(request, text_status, error) {
+				alert(error);
+			}
+		});
+
+	} else if (selection == 1) {
+		// user has selected new account
+		result = true;
+
+	} else {
+		// remove password fields
+		var container = $('div#input_details div.page').eq(1);
+			
+		container.find('hr').eq(1).remove();
+		container.find('input[name=password], input[name=password_confirm]').parent().remove();
+
+		// user has selected guest checkout
+		result = true;
+	}
+
+	return result;
+}
+
+/**
+ * Validate shipping information page.
+ *
+ * @return boolean
+ */
+function validateShippingInformationPage() {
+	var container = $('div#shipping_information');
+	var fields = container.find('input,select');
+
+	fields.each(function(index) {
+		var field = $(this);
+
+		if (field.data('required') == 1 && field.val() == '') 
+			field.addClass('bad'); else
+			field.removeClass('bad');
+	});
+
+	return container.find('.bad').length == 0;
+}
+
+/**
+ * Validate payment method page.
+ *
+ * @return boolean
+ */
+function validatePaymentMethodPage() {
+	var methods = $('div.payment_methods');
+	var result = methods.find('span.active').length > 0;
+
+	if (!result)
+		methods.find('span').addClass('bad');
+
+	return result;
+}
+
+/**
+ * Validate billing information page.
+ *
+ * @return boolean
+ */
+function validateBillingInformationPage() {
+	var container = $('div#billing_information');
+	var fields = container.find('input,select');
+
+	fields.each(function(index) {
+		var field = $(this);
+
+		if (field.data('required') == 1 && field.val() == '') 
+			field.addClass('bad'); else
+			field.removeClass('bad');
+	});
+
+	return container.find('.bad').length == 0;
+}
+
+/**
+ * Validate form pages.
+ *
+ * @param integer current_page
+ * @param integer new_page
+ * @return boolean
+ */
+function validatePage(current_page, new_page) {
+	var result = true;
+
+	switch(current_page) {
+		case 0:
+			// validate sign in page
+			result = validateSignInPage();
+			break;
+
+		case 1:
+			// validate shipping information page
+			result = validateShippingInformationPage();
+			break;
+
+		case 2:
+			// validate payment method page
+			result = validatePaymentMethodPage();
+			break;
+
+		case 3:
+			// validate billing information
+			result = validateBillingInformationPage();
+			break;
+	}
+
+	return result;
+}
+
 // implement page control
 var page_control = new PageControl('div#input_details div.pages');
 
@@ -5,7 +153,8 @@ page_control
 	.setAllowForward(false)
 	.setSubmitOnEnd(true)
 	.attachControls('div#checkout_stepps a')
-	.attachForm('div#input_details form');
+	.attachForm('div#input_details form')
+	.connect('page-flip', validatePage);
 
 // implement payment method selection
 var methods = $('div.payment_methods span');
@@ -21,64 +170,11 @@ methods.click(function(event) {
 	methods.not(method).removeClass('active');
 	method.addClass('active');
 
+	// remove bad class
+	methods.removeClass('bad');
+
 	// disable billing information page if payment method provides info about buyer
 	if (method.data('provides-information') == 1)
 		page_control.disablePage(3); else
 		page_control.enablePage(3);
 });
-
-// implement sign in form
-var sign_in_form = $('div#sign_in');
-var next_button = sign_in_form.find('button.next');
-var backend_url = $('base').attr('href') + '/index.php';
-var email_field = sign_in_form.find('input[name=email]');
-var password_field = sign_in_form.find('input[name=password]');
-
-next_button
-	.off('click')
-	.click(function(event) {
-		// prevent default button behavior
-		event.preventDefault();
-
-		// check which option is selected
-		var selection = sign_in_form.find('input[name=existing_user]:checked').val();
-
-		// load data from server if needed
-		if (selection == 0) {
-			var data = {
-					section: 'shop',
-					action: 'json_get_user_info',
-					email: email_field.val(),
-					password: password_field.val()
-				};
-
-			$.ajax({
-				url: backend_url,
-				type: 'GET',
-				async: false,
-				data: data,
-				dataType: 'json',
-				context: this,
-				success: function(data) {
-					page_control._handleNext(event);
-				}, 
-				error: function(request, text_status, error) {
-					alert(error);
-				}
-			});
-
-		} else if (selection == 1) {
-			// user has selected new account
-			page_control._handleNext(event);
-
-		} else {
-			// remove password fields
-			var container = $('div#input_details div.page').eq(1);
-				
-			container.find('hr').eq(1).remove();
-			container.find('input[name=password], input[name=password_confirm]').parent().remove();
-
-			// user has selected guest checkout
-			page_control._handleNext(event);
-		}
-	});
