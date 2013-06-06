e1e3b6b4433248bceee73ac17b4dd373f37e482d
MeanEYE.rcf <meaneye.rcf@gmail.com>
2013-06-06 17:20:31 +0300

    Modules: Shop: Moved checkout.js file to buyer_information.js as this name better describes the purpose of code.

--- modules/shop/include/buyer_information.js
+++ modules/shop/include/buyer_information.js
@@ -1,4 +1,12 @@
 /**
+ * Buyers Information JavaScript Implementation
+ *
+ * Copyright (c) 2013. by Way2CU
+ * Author: Mladen Mijatov
+ */
+
+
+/**
  * Validate sign in page.
  *
  * @return boolean
@@ -18,7 +26,7 @@ function validateSignInPage() {
 	if (selection == 0) {
 		var data = {
 				section: 'shop',
-				action: 'json_get_user_info',
+				action: 'json_get_account_info',
 				email: email_field.val(),
 				password: password_field.val()
 			};
@@ -62,9 +70,12 @@ function validateSignInPage() {
  * @return boolean
  */
 function validateShippingInformationPage() {
+	var sign_in_form = $('div#sign_in');
 	var container = $('div#shipping_information');
 	var fields = container.find('input,select');
+	var backend_url = $('base').attr('href') + '/index.php';
 
+	// add "bad" class to every required field which is empty
 	fields.each(function(index) {
 		var field = $(this);
 
@@ -72,6 +83,42 @@ function validateShippingInformationPage() {
 			field.addClass('bad'); else
 			field.removeClass('bad');
 	});
+
+	// make sure passwords match
+	var password = container.find('input[name=password]');
+	var password_confirm = container.find('input[name=password_confirm]');
+
+	if (password.length > 0 && password_confirm.length > 0 && password.val() != password_confirm.val()) {
+		password.addClass('bad');
+		password_confirm.addClass('bad');
+	}
+
+	// check if account with specified email already exists
+	var email_field = container.find('input[name=email]');
+	var existing_user = sign_in_form.find('input[name=existing_user]:checked').val();
+
+	if (existing_user == 1) {
+		var data = {
+				section: 'shop',
+				action: 'json_get_account_exists',
+				email: email_field.val()
+			};
+
+		$.ajax({
+			url: backend_url,
+			type: 'GET',
+			async: false,
+			data: data,
+			dataType: 'json',
+			context: this,
+			success: function(data) {
+				if (data.account_exists) {
+					email_field.addClass('bad')
+					alert(data.message);
+				}
+			}
+		});
+	}
 
 	return container.find('.bad').length == 0;
 }
@@ -146,35 +193,37 @@ function validatePage(current_page, new_page) {
 	return result;
 }
 
-// implement page control
-var page_control = new PageControl('div#input_details div.pages');
+$(function() {
+	// implement page control
+	var page_control = new PageControl('div#input_details div.pages');
 
-page_control
-	.setAllowForward(false)
-	.setSubmitOnEnd(true)
-	.attachControls('div#checkout_stepps a')
-	.attachForm('div#input_details form')
-	.connect('page-flip', validatePage);
+	page_control
+		.setAllowForward(false)
+		.setSubmitOnEnd(true)
+		.attachControls('div#checkout_stepps a')
+		.attachForm('div#input_details form')
+		.connect('page-flip', validatePage);
 
-// implement payment method selection
-var methods = $('div.payment_methods span');
-var method_field = $('input[name=payment_method]');
+	// implement payment method selection
+	var methods = $('div.payment_methods span');
+	var method_field = $('input[name=payment_method]');
 
-methods.click(function(event) {
-	var method = $(this);
+	methods.click(function(event) {
+		var method = $(this);
 
-	// set payment method before processing
-	method_field.val(method.data('name'));
-		
-	// add selection class to 
-	methods.not(method).removeClass('active');
-	method.addClass('active');
+		// set payment method before processing
+		method_field.val(method.data('name'));
+			
+		// add selection class to 
+		methods.not(method).removeClass('active');
+		method.addClass('active');
 
-	// remove bad class
-	methods.removeClass('bad');
+		// remove bad class
+		methods.removeClass('bad');
 
-	// disable billing information page if payment method provides info about buyer
-	if (method.data('provides-information') == 1)
-		page_control.disablePage(3); else
-		page_control.enablePage(3);
+		// disable billing information page if payment method provides info about buyer
+		if (method.data('provides-information') == 1)
+			page_control.disablePage(3); else
+			page_control.enablePage(3);
+	});
 });
