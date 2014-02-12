3a53813b2b89b6ea5a440f35f9c4b3515b528eea
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-02-12 11:41:08 +0200

    Shop: Transactions manager: Removed unneeded fields.

--- modules/shop/units/shop_transactions_manager.php
+++ modules/shop/units/shop_transactions_manager.php
@@ -22,10 +22,6 @@ class ShopTransactionsManager extends ItemManager {
 		$this->addProperty('type', 'smallint');
 		$this->addProperty('status', 'smallint');
 		$this->addProperty('currency', 'int');
-		$this->addProperty('trial', 'int');
-		$this->addProperty('trial_count', 'int');
-		$this->addProperty('interval', 'int');
-		$this->addProperty('interval_count', 'int');
 		$this->addProperty('handling', 'decimal');
 		$this->addProperty('shipping', 'decimal');
 		$this->addProperty('weight', 'decimal');
@@ -35,8 +31,6 @@ class ShopTransactionsManager extends ItemManager {
 		$this->addProperty('token', 'varchar');
 		$this->addProperty('total', 'decimal');
 		$this->addProperty('timestamp', 'timestamp');
-		$this->addProperty('start_time', 'timestamp');
-		$this->addProperty('end_time', 'timestamp');
 	}
 
 	/**
