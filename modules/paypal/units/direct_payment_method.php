4d9fb500d9b4ef29b350825c5b9b4dcbaa4e0553
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-03-01 16:28:51 +0200

    PayPal: Add recurring plan support for direct payment method.

--- modules/paypal/units/direct_payment_method.php
+++ modules/paypal/units/direct_payment_method.php
@@ -12,6 +12,21 @@
 class PayPal_Direct extends PaymentMethod {
 	private static $_instance;
 
+	private $units = array(
+		RecurringPayment::DAY	=> 'Day',
+		RecurringPayment::WEEK	=> 'Week',
+		RecurringPayment::MONTH	=> 'Month',
+		RecurringPayment::YEAR	=> 'Year'
+	);
+
+	private $card_type = array(
+			CardType::VISA => 'Visa',
+			CardType::MASTERCARD => 'MasterCard',
+			CardType::DISCOVER => 'Discover',
+			CardType::AMERICAN_EXPRESS => 'Amex',
+			CardType::MAESTRO => 'Maestro'
+		);
+
 	/**
 	 * Constructor
 	 */
@@ -56,7 +71,7 @@ class PayPal_Direct extends PaymentMethod {
 	 * @return string
 	 */
 	public function get_url() {
-		return '';
+		return url_Make('direct-checkout', 'paypal');
 	}
 
 	/**
@@ -88,7 +103,8 @@ class PayPal_Direct extends PaymentMethod {
 	 * @return array
 	 */
 	public function get_recurring_plans() {
-		return array();
+		$method = PayPal_Express::getInstance($this->parent);
+		return $method->get_recurring_plans();
 	}
 
 	/**
@@ -124,7 +140,25 @@ class PayPal_Direct extends PaymentMethod {
 	 * @return string
 	 */
 	public function new_recurring_payment($plan_name, $billing_information, $return_url, $cancel_url) {
-		return '';
+		$result = '';
+		$manager = PayPal_PlansManager::getInstance();
+		$plan = $manager->getSingleItem($manager->getFieldNames(), array('text_id' => $plan_name));
+
+		if (is_object($plan)) {
+			$params = array(
+					'plan_name'		=> $plan_name,
+					'return_url'	=> $return_url,
+					'type'			=> 'recurring'
+				);
+
+			foreach ($billing_information as $key => $value)
+				$params[$key] = $value;
+
+			foreach ($params as $name => $value)
+				$result .= '<input type="hidden" name="'.$name.'" value="'.$value.'">';
+		}
+
+		return $result;
 	}
 
 	/**
@@ -137,6 +171,111 @@ class PayPal_Direct extends PaymentMethod {
 		$result = false;
 
 		return $result;
+	}
+
+	/**
+	 * Complete checkout and charge money.
+	 */
+	public function completeCheckout() {
+		global $language;
+
+		$shop = shop::getInstance();
+		$return_url = fix_chars($_REQUEST['return_url']);
+		$recurring = isset($_REQUEST['type']) && $_REQUEST['type'] == 'recurring';
+		$transaction_uid = $_SESSION['transaction']['uid'];
+
+		// get billing information
+		$billing = array();
+		$fields = array(
+			'billing_full_name', 'billing_card_type', 'billing_credit_card', 'billing_expire_month',
+			'billing_expire_year', 'billing_cvv'
+		);
+
+		foreach($fields as $field)
+			if (isset($_REQUEST[$field]))
+				$billing[$field] = fix_chars($_REQUEST[$field]);
+
+		// create recurring profile
+		if ($recurring) {
+			$plan_name = $_SESSION['recurring_plan'];
+			$manager = PayPal_PlansManager::getInstance();
+			$plan = $manager->getSingleItem($manager->getFieldNames(), array('text_id' => $plan_name));
+			$request_id = 0;
+
+			// generate params for description
+			$plan_params = array(
+				'price'			=> $plan->price,
+				'period'		=> $plan->interval_count,
+				'unit'			=> $plan->interval,
+				'setup'			=> $plan->setup_price,
+				'trial_period'	=> $plan->trial_count,
+				'trial_unit'	=> $plan->trial
+			);
+
+			// charge one time setup fee
+			// TODO: Charge one time setup fee.
+
+			// create recurring payments profile
+			$recurring_fields = $fields;
+
+			// set buyer information
+			$name = explode(' ', $billing['billing_full_name']);
+			$recurring_fields['CREDITCARDTYPE'] = $this->card_type[$billing['billing_card_type']];
+			$recurring_fields['ACCT'] = $billing['billing_credit_card'];
+			$recurring_fields['EXPDATE'] = $billing['billing_expire_month'].$billing['billing_expire_year'];
+			$recurring_fields['FIRSTNAME'] = $name[0];
+			$recurring_fields['LASTNAME'] = $name[1];
+
+			// set starting date of the profile
+			$start_timestamp = strtotime($plan->start_time);
+			if ($start_timestamp < time())
+				$start_timestamp = time();
+
+			$recurring_fields['PROFILESTARTDATE'] = strftime('%Y-%m-%dT%T%z', $start_timestamp);
+
+			// set description
+			$recurring_fields['DESC'] = $shop->formatRecurring($plan_params);
+
+			// set currency
+			$recurring_fields['AMT'] = $plan->price;
+			$recurring_fields['CURRENCYCODE'] = $shop->getDefaultCurrency();
+
+			// billing period
+			$recurring_fields['BILLINGPERIOD'] = $this->units[$plan->interval];
+			$recurring_fields['BILLINGFREQUENCY'] = $plan->interval_count;
+
+			// trial period
+			if ($plan->trial_count > 0) {
+				$recurring_fields['TRIALBILLINGPERIOD'] = $this->units[$plan->trial];
+				$recurring_fields['TRIALBILLINGFREQUENCY'] = $plan->trial_count;
+				$recurring_fields['TRIALTOTALBILLINGCYCLES'] = 1;
+			}
+
+			// make api call
+			$response = PayPal_Helper::callAPI(
+								PayPal_Helper::METHOD_CreateRecurringPaymentsProfile,
+								$recurring_fields
+							);
+
+			if ($response['ACK'] == 'Success' || $response['ACK'] == 'SuccessWithWarning') {
+				// update transaction token
+				$shop->setTransactionToken($transaction_uid, fix_chars($response['PROFILEID']));
+
+				// update transaction status
+				if ($response['PROFILESTATUS'] == 'ActiveProfile')
+					$shop->setTransactionStatus($transaction_uid, TransactionStatus::COMPLETED);
+
+			} else {
+				// report error
+				$error_code = urldecode($response['L_ERRORCODE0']);
+				$error_long = urldecode($response['L_LONGMESSAGE0']);
+
+				trigger_error("PayPal_Express: ({$error_code}) - {$error_long}", E_USER_ERROR);
+			}
+
+			// redirect user
+			header('Location: '.$return_url, true, 302);
+		}
 	}
 }
 
