6392a256bce4f9422f5bf23643140b8a3cebc17a
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-06-23 17:01:21 +0200

    Core: Event handler: Made EventHandler static class.

--- units/event_handler.php
+++ units/event_handler.php
@@ -8,66 +8,74 @@
  *
  * Author: Mladen Mijatov
  */
+namespace Core;
 
-class EventAlreadyExistsError extends Exception {};
-class UnknownEventError extends Exception {};
-class InvalidParamCountError extends Exception {};
+class EventAlreadyExistsError extends \Exception {};
+class UnknownEventError extends \Exception {};
+class InvalidParamCountError extends \Exception {};
 
 
-final class EventHandler {
-	private $event_names = array();
-	private $callbacks = array();
-
-	public function __construct() {
-	}
+final class Events {
+	private static $event_names = array();
+	private static $callbacks = array();
 
 	/**
 	 * Register event and its parameters.
 	 *
+	 * @param string $module
 	 * @param string $event_name
 	 * @param integer $required_param_count
 	 */
-	public function registerEvent($event_name, $required_param_count=0) {
-		if (!isset($this->event_names[$event_name])) {
-			$this->event_names[$event_name] = $required_param_count; 
-			$this->callbacks[$event_name] = array();
+	public static function register($module, $event_name, $required_param_count=0) {
+		// create storage for module
+		if (!isset(self::$event_names[$module])) {
+			self::$event_names[$module] = array();
+			self::$callbacks[$module] = array();
+		}
+
+		if (!isset(self::$event_names[$module][$event_name])) {
+			self::$event_names[$module][$event_name] = $required_param_count; 
+			self::$callbacks[$module][$event_name] = array();
 
 		} else {
-			throw new EventAlreadyExistsError("Unable to register '{$event_name}' event.");
+			throw new EventAlreadyExistsError("Unable to register module '{$module}' eventl '{$event_name}'.");
 		}
 	}
 
 	/**
 	 * Connect specified callback to event.
 	 *
+	 * @param string $module
 	 * @param string $event_name
 	 * @param callable $callback
 	 * @param object $object [optional]
 	 */
-	public function connect($event_name, $callback, $object=null) {
+	public static function connect($module, $event_name, $callback, $object=null) {
 		$callable = is_null($object) ? $callback : array($object, $callback);
 
-		if (isset($this->event_names[$event_name]))
-			$this->callbacks[$event_name][] = $callable; else
-			throw new UnknownEventError("Unable to connect to '{$event_name}' event.");
+		if (isset(self::$event_names[$module][$event_name]))
+			self::$callbacks[$module][$event_name][] = $callable; else
+			throw new UnknownEventError("Unable to connect to module's '{$module}' event '{$event_name}'.");
 	}
 
 	/**
 	 * Trigger event by its name and pass all additional arguments.
 	 *
+	 * @param string $module
 	 * @param string $event_name
 	 * @param ...
 	 * @return array
 	 */
-	public function trigger($event_name) {
-		if (!isset($this->event_names[$event_name]))
-			throw new UnknownEventError("Unable to trigger event ({$event_name}).");
+	public static function trigger($module, $event_name) {
+		if (!isset(self::$event_names[$module][$event_name]))
+			throw new UnknownEventError("Unable to trigger module '{$module}' event '{$event_name}'.");
 
 		$result = array();
-		$required_param_count = $this->event_names[$event_name];
+		$required_param_count = self::$event_names[$module][$event_name];
 
 		// get function parameters
 		$params = func_get_args();
+		array_shift($params);
 		array_shift($params);
 		$param_count = count($params);
 
@@ -75,11 +83,11 @@ final class EventHandler {
 		if ($param_count < $required_param_count)
 			throw new InvalidParamCountError(
 				"Invalid number of parameters specified ({$param_count}) ".
-				"when triggering event '{$event_name}'. Required: {$required_param_count}"
+				"when triggering module '{$module}' event '{$event_name}'. Required: {$required_param_count}"
 			);
 
 		// call all callback methods
-		foreach ($this->callbacks[$event_name] as $callable)
+		foreach (self::$callbacks[$module][$event_name] as $callable)
 			$result[] = call_user_func_array($callable, $params);
 
 		return $result;
