f9bde590db6597f6bb3b6659b2f2e2b268670b3e
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-06-25 16:39:21 +0200

    Libraries: Add Mailchimp.

---
+++ libraries/mailchimp/units/mobile.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Library\Mailchimp;
+
+
+class Mailchimp_Mobile {
+    public function __construct(Mailchimp $master) {
+        $this->master = $master;
+    }
+
+}
+
+
---
+++ libraries/mailchimp/units/neapolitan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Library\Mailchimp;
+
+
+class Mailchimp_Neapolitan {
+    public function __construct(Mailchimp $master) {
+        $this->master = $master;
+    }
+
+}
+
+
