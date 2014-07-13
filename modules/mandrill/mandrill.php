90fa858b32eeb66e07fa3bdc2d6189fb9e9b965d
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-07-13 17:39:24 +0200

    Added Mandrill support module.

---
+++ modules/mandrill/mandrill.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * Module Template
+ *
+ * This module is a template to make process of starting a development of new module
+ * fast and painless. This code reflects the state of system in general and should be
+ * kept up-to-date with remainder of the system.
+ *
+ * Author: Mladen Mijatov
+ */
+use Core\Module;
+
+require_once('units/mailer.php');
+
+
+class mandrill extends Module {
+	private static $_instance;
+
+	/**
+	 * Constructor
+	 */
+	protected function __construct() {
+		global $section;
+		
+		parent::__construct(__FILE__);
+
+		// register mailer
+		if (class_exists('contact_form')) {
+			$mailer = new Mandrill_Mailer($this->language, $this->settings['api_key']);
+
+			$contact_form = contact_form::getInstance();
+			$contact_form->registerMailer('mandrill', $mailer);
+		}
+
+		// register backend
+		if (class_exists('backend') && $section == 'backend') {
+			$backend = backend::getInstance();
+
+			$mandrill_menu = new backend_MenuItem(
+					$this->getLanguageConstant('menu_mandrill'),
+					url_GetFromFilePath($this->path.'images/icon.svg'),
+					'javascript:void(0);',
+					$level=5
+				);
+
+			$mandrill_menu->addChild('', new backend_MenuItem(
+								$this->getLanguageConstant('menu_settings'),
+								url_GetFromFilePath($this->path.'images/settings.svg'),
+
+								window_Open( // on click open window
+											'mandrill_settings',
+											370,
+											$this->getLanguageConstant('title_settings'),
+											true, true,
+											backend_UrlMake($this->name, 'settings')
+										),
+								$level=5
+							));
+
+			$backend->addMenu($this->name, $mandrill_menu);
+		}
+	}
+
+	/**
+	 * Public function that creates a single instance
+	 */
+	public static function getInstance() {
+		if (!isset(self::$_instance))
+			self::$_instance = new self();
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Transfers control to module functions
+	 *
+	 * @param array $params
+	 * @param array $children
+	 */
+	public function transferControl($params = array(), $children = array()) {
+		// global control actions
+		if (isset($params['backend_action']))
+			switch ($params['backend_action']) {
+				case 'settings':
+					$this->showSettings();
+					break;
+
+				case 'settings_save':
+					$this->saveSettings();
+					break;
+
+				default:
+					break;
+			}
+	}
+
+	/**
+	 * Event triggered upon module initialization
+	 */
+	public function onInit() {
+		$this->saveSetting('api_key', '');
+	}
+
+	/**
+	 * Event triggered upon module deinitialization
+	 */
+	public function onDisable() {
+	}
+
+	/**
+	 * Show settings form.
+	 */
+	private function showSettings() {
+		$template = new TemplateHandler('settings.xml', $this->path.'templates/');
+		$template->setMappedModule($this->name);
+
+		$params = array(
+						'form_action'	=> backend_UrlMake($this->name, 'settings_save'),
+						'cancel_action'	=> window_Close('mandrill_settings')
+					);
+
+		$template->restoreXML();
+		$template->setLocalParams($params);
+		$template->parse();
+	}
+
+	/**
+	 * Save new settings.
+	 */
+	private function saveSettings() {
+		// save setting
+		$this->saveSetting('api_key', fix_chars($_REQUEST['api_key']));
+
+		// show message
+		$template = new TemplateHandler('message.xml', $this->path.'templates/');
+		$template->setMappedModule($this->name);
+
+		$params = array(
+					'message'	=> $this->getLanguageConstant('message_saved'),
+					'button'	=> $this->getLanguageConstant('close'),
+					'action'	=> window_Close('mandrill_settings')
+				);
+
+		$template->restoreXML();
+		$template->setLocalParams($params);
+		$template->parse();
+	}
+}
---
+++ modules/mandrill/units/mailer.php
@@ -0,0 +1,242 @@
+<?php
+
+use Library\Mandrill\Mandrill as API;
+use Library\Mandrill\Mandrill_Error as API_Error;
+
+
+class Mandrill_Mailer extends ContactForm_Mailer {
+	private $language;
+	private $variables;
+	private $plain_body;
+	private $html_body;
+	private $subject;
+
+	private $message = null;
+	private $api_key = null;
+
+	private $accepted_status = array('sent', 'queued', 'scheduled');
+
+	public function __construct($language, $api_key) {
+		$this->language = $language;
+		$this->api_key = $api_key;
+	}
+
+	/**
+	 * Replace placeholders with field values in specified template.
+	 *
+	 * @param string $template
+	 * @param array $fields
+	 * @return string
+	 */
+	private function replace_fields($template) {
+		$keys = array_keys($this->variables);
+		$values = array_values($this->variables);
+
+		// preformat keys for replacement
+		foreach ($keys as $index => $key)
+			$keys[$index] = "%{$key}%";
+
+		// replace field place holders with values
+		return str_replace($keys, $values, $template);
+	}
+
+	/**
+	* Get localized name.
+	*
+	* @return string
+	*/
+	public function get_title() {
+		return $this->language->getText('mailer_title');
+	}
+
+	/**
+	* Prepare mailer for sending new message. This function
+	* is ideal place to prepare to initialize internal storage
+	* variables. No connections should be established at this
+	* point to avoid potential timeouts.
+	*/
+	public function start_message() {
+		$this->message = array();
+		$this->variables = array();
+		$this->plain_body = null;
+		$this->html_body = null;
+	}
+
+	/**
+	* Finalize message and send it to specified addresses.
+	* 
+	* Note: Before sending, you *must* check if contact_form
+	* function detectBots returns false.
+	*
+	* @return boolean
+	*/
+	public function send() {
+		// replace variables for message body and subject
+		$this->message['subject'] = $this->replace_fields($this->subject);
+		$this->message['html'] = $this->replace_fields($this->html_body);
+		$this->message['text'] = $this->replace_fields($this->plain_body);
+
+		// send message
+		try {
+			$mandrill = new API($this->api_key);
+			$response = $mandrill->messages->send($this->message);
+
+		} catch (API_Error $error) { 
+			trigger_error(get_class($error).': '.$error->getMessage(), E_USER_WARNING);
+		}
+
+		// prepare result
+		$result = in_array($response['status'], $this->accepted_status);
+
+		return $result;
+	}
+
+	/**
+	* Set sender of message.
+	*
+	* @param string $address
+	* @param string $name
+	*/
+	public function set_sender($address, $name=null) {
+		$this->message['from_email'] = $address;
+		if (!is_null($name))
+			$this->message['from_name'] = $name;
+	}
+
+	/**
+	* Add recipient for the message. Recipient name is optional.
+	*
+	* @param string $address
+	* @param string $name
+	*/
+	public function add_recipient($address, $name=null) {
+		// make sure there's a list of recipients
+		if (!isset($this->message['to']))
+			$this->message['to'] = array();
+
+		// create recipient array
+		$recipient = array(
+				'email'	=> $address,
+				'type'	=> 'to'
+			);
+
+		if (!is_null($name))
+			$recipient['name'] = $name;
+
+		// add recipient
+		$this->message['to'][] = $recipient;
+	}
+
+	/**
+	* Add recipient to carbon copy (CC) field. Name is optional.
+	*
+	* @param string $address
+	* @param string $name
+	*/
+	public function add_cc_recipient($address, $name=null) {
+		// make sure there's a list of recipients
+		if (!isset($this->message['to']))
+			$this->message['to'] = array();
+
+		// create recipient array
+		$recipient = array(
+				'email'	=> $address,
+				'type'	=> 'cc'
+			);
+
+		if (!is_null($name))
+			$recipient['name'] = $name;
+
+		// add recipient
+		$this->message['to'][] = $recipient;
+	}
+
+	/**
+	* Add recipient to blind carbon copy (BCC) field. Name is optional.
+	*
+	* @param string $address
+	* @param string $name
+	*/
+	public function add_bcc_recipient($address, $name=null) {
+		// make sure there's a list of recipients
+		if (!isset($this->message['to']))
+			$this->message['to'] = array();
+
+		// create recipient array
+		$recipient = array(
+				'email'	=> $address,
+				'type'	=> 'bcc'
+			);
+
+		if (!is_null($name))
+			$recipient['name'] = $name;
+
+		// add recipient
+		$this->message['to'][] = $recipient;
+	}
+
+	/**
+	* Set message subject.
+	*
+	* @param string $subject
+	*/
+	public function set_subject($subject) {
+		$this->subject = $subject;
+	}
+
+	/**
+	* Set variables to be replaced in subject and body.
+	*
+	* @param array $params
+	*/
+	public function set_variables($variables) {
+		$this->variables = $variables;
+	}
+
+	/**
+	* Set message body. HTML body is optional.
+	*
+	* @param string $plain_body
+	* @param string $html_body
+	*/
+	public function set_body($plain_body, $html_body=null) {
+		$this->plain_body = $plain_body;
+		if (!is_null($html_body) && !empty($html_body))
+			$this->html_body = $html_body;
+	}
+
+	/**
+	* Attach file to message. Inline attachments will have image name
+	* set as "Content-ID". Inline files can be addressed in HTML body
+	* like this:
+	*
+	* <img src="cid:example_file.png">
+	*
+	* @param string $file_name
+	* @param boolean $inline
+	*/
+	public function attach_file($file_name, $inline=false) {
+		// encode data
+		$data = file_get_contents($file_name);
+		$data = base64_encode($data);
+
+		// get file mime type
+		$handle = finfo_open(FILEINFO_MIME_TYPE);
+		$mime_type = finfo_file($handle, $file_name);
+		finfo_close($handle);
+
+		// create structure
+		$structure = array(
+				'type'		=> $mime_type,
+				'name'		=> basename($file_name),
+				'content'	=> $data
+			);
+
+		// attach file
+		if ($inline)
+			$this->message['images'][] = $structure; else
+			$this->message['attachments'][] = $structure;
+	}
+}
+
+?>
