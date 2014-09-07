0e57a5aedfe3c9072537322403f7dd037229f212
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-09-07 20:37:24 +0300

    Cache: Implemented file system cache.

--- units/cache/file_provider.php
+++ units/cache/file_provider.php
@@ -17,6 +17,15 @@ class FileProvider implements Provider {
 	 * Initialization function.
 	 */
 	public function initialize() {
+		global $cache_path, $cache_expire_period;
+
+		$file_list = glob($cache_path.'*.cache');
+		$time_limit = time() - $cache_expire_period;
+
+		// remove files which passed the limit
+		foreach ($file_list as $file_name)
+			if (filemtime($file_name) < $time_limit)
+				unlink($file_name);
 	};
 
 	/**
@@ -28,7 +37,18 @@ class FileProvider implements Provider {
 	 * @param string $data
 	 * @param int $expires
 	 */
-	public function store_data($uid, $data, $expires) {
+	public function storeData($uid, $data, $expires) {
+		global $cache_path;
+
+		// prepare for storing
+		$file_name = $cache_path.$uid.'.cache';
+
+		// make sure we are not overwriting a file
+		if (file_exists($file_name))
+			unlink($file_name);
+
+		// store content
+		file_put_contents($file_name, $data);
 	};
 
 	/**
@@ -39,7 +59,18 @@ class FileProvider implements Provider {
 	 * @param string $uid
 	 * @return mixed
 	 */
-	public function get_data($uid) {
+	public function getData($uid) {
+		global $cache_path, $cache_expire_period;
+
+		$result = null;
+		$file_name = $cache_path.$uid.'.cache';
+		$time_limit = time() - $cache_expire_period;
+
+		// load file content only if cache is still valid
+		if (file_exists($file_name) && filemtime($file_name) > $time_limit)
+			$result = file_get_contents($file_name);
+
+		return $result;
 	};
 
 	/**
@@ -48,13 +79,21 @@ class FileProvider implements Provider {
 	 * @param string $uid
 	 * @return boolean
 	 */
-	function is_cached($uid) {
+	function isCached($uid) {
+		global $cache_path;
+
+		$file_name = $cache_path.$uid.'.cache';
+		$time_limit = time() - $cache_expire_period;
+
+		return = file_exists($file_name) && filemtime($file_name) > $time_limit;
 	};
 
 	/**
 	 * Clear all cache.
 	 */
-	function clear_cache() {
+	function clearCache() {
+		global $cache_path;
+		array_map('unlink', glob($cache_path.'*.cache'));
 	};
 }
 
