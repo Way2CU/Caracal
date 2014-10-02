2befaa1c8b061f9e51c701019b4382ca3b1cf93b
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-10-02 12:38:06 +0300

    Cache manager: Set permissions when creating directory.

--- units/cache/cache.php
+++ units/cache/cache.php
@@ -43,15 +43,16 @@ class Manager {
 		global $cache_path, $optimize_code, $cache_method, $section;
 
 		// decide if we should cache current page
-		$this->should_cache = 
-					$cache_method != Type::NONE && 
-					!($section == 'backend' || $section == 'backend_module') && 
+		$this->should_cache =
+					$cache_method != Type::NONE &&
+					!($section == 'backend' || $section == 'backend_module') &&
 					$_SERVER['REQUEST_METHOD'] == 'GET' &&
 					!_AJAX_REQUEST;
 
 		// make sure cache directory exists
 		if (($optimize_code || $cache_method != Type::NONE) && !file_exists($cache_path))
-			mkdir($cache_path);
+			if (mkdir($cache_path, 0775, true) === false)
+				trigger_error('Cache manager: Unable to create storage path.', E_USER_WARNING);
 
 		// create cache provider
 		switch ($cache_method) {
