ffe81d33a6d75145e70a3ff0e40981a099edb9ee
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-10-14 18:09:52 +0300

    Mandrill: Implemented add_header_string to mailer.

--- modules/mandrill/units/mailer.php
+++ modules/mandrill/units/mailer.php
@@ -42,20 +42,20 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 	}
 
 	/**
-	* Get localized name.
-	*
-	* @return string
-	*/
+	 * Get localized name.
+	 *
+	 * @return string
+	 */
 	public function get_title() {
 		return $this->language->getText('mailer_title');
 	}
 
 	/**
-	* Prepare mailer for sending new message. This function
-	* is ideal place to prepare to initialize internal storage
-	* variables. No connections should be established at this
-	* point to avoid potential timeouts.
-	*/
+	 * Prepare mailer for sending new message. This function
+	 * is ideal place to prepare to initialize internal storage
+	 * variables. No connections should be established at this
+	 * point to avoid potential timeouts.
+	 */
 	public function start_message() {
 		$this->message = array();
 		$this->variables = array();
@@ -64,13 +64,13 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 	}
 
 	/**
-	* Finalize message and send it to specified addresses.
-	* 
-	* Note: Before sending, you *must* check if contact_form
-	* function detectBots returns false.
-	*
-	* @return boolean
-	*/
+	 * Finalize message and send it to specified addresses.
+	 *
+	 * Note: Before sending, you *must* check if contact_form
+	 * function detectBots returns false.
+	 *
+	 * @return boolean
+	 */
 	public function send() {
 		// replace variables for message body and subject
 		$this->message['subject'] = $this->replace_fields($this->subject);
@@ -82,7 +82,7 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 			$mandrill = new API($this->api_key);
 			$response = $mandrill->messages->send($this->message);
 
-		} catch (API_Error $error) { 
+		} catch (API_Error $error) {
 			trigger_error(get_class($error).': '.$error->getMessage(), E_USER_WARNING);
 		}
 
@@ -103,11 +103,11 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 	}
 
 	/**
-	* Set sender of message.
-	*
-	* @param string $address
-	* @param string $name
-	*/
+	 * Set sender of message.
+	 *
+	 * @param string $address
+	 * @param string $name
+	 */
 	public function set_sender($address, $name=null) {
 		$this->message['from_email'] = $address;
 		if (!is_null($name))
@@ -115,11 +115,11 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 	}
 
 	/**
-	* Add recipient for the message. Recipient name is optional.
-	*
-	* @param string $address
-	* @param string $name
-	*/
+	 * Add recipient for the message. Recipient name is optional.
+	 *
+	 * @param string $address
+	 * @param string $name
+	 */
 	public function add_recipient($address, $name=null) {
 		// make sure there's a list of recipients
 		if (!isset($this->message['to']))
@@ -127,9 +127,9 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 
 		// create recipient array
 		$recipient = array(
-				'email'	=> $address,
-				'type'	=> 'to'
-			);
+			'email'	=> $address,
+			'type'	=> 'to'
+		);
 
 		if (!is_null($name))
 			$recipient['name'] = $name;
@@ -139,11 +139,11 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 	}
 
 	/**
-	* Add recipient to carbon copy (CC) field. Name is optional.
-	*
-	* @param string $address
-	* @param string $name
-	*/
+	 * Add recipient to carbon copy (CC) field. Name is optional.
+	 *
+	 * @param string $address
+	 * @param string $name
+	 */
 	public function add_cc_recipient($address, $name=null) {
 		// make sure there's a list of recipients
 		if (!isset($this->message['to']))
@@ -151,9 +151,9 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 
 		// create recipient array
 		$recipient = array(
-				'email'	=> $address,
-				'type'	=> 'cc'
-			);
+			'email'	=> $address,
+			'type'	=> 'cc'
+		);
 
 		if (!is_null($name))
 			$recipient['name'] = $name;
@@ -163,11 +163,11 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 	}
 
 	/**
-	* Add recipient to blind carbon copy (BCC) field. Name is optional.
-	*
-	* @param string $address
-	* @param string $name
-	*/
+	 * Add recipient to blind carbon copy (BCC) field. Name is optional.
+	 *
+	 * @param string $address
+	 * @param string $name
+	 */
 	public function add_bcc_recipient($address, $name=null) {
 		// make sure there's a list of recipients
 		if (!isset($this->message['to']))
@@ -175,9 +175,9 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 
 		// create recipient array
 		$recipient = array(
-				'email'	=> $address,
-				'type'	=> 'bcc'
-			);
+			'email'	=> $address,
+			'type'	=> 'bcc'
+		);
 
 		if (!is_null($name))
 			$recipient['name'] = $name;
@@ -187,29 +187,44 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 	}
 
 	/**
-	* Set message subject.
-	*
-	* @param string $subject
-	*/
+	 * Add custom header string.
+	 *
+	 * @param string $key
+	 * @param string $value
+	 */
+	public function add_header_string($key, $value) {
+		// make sure there's a list of recipients
+		if (!isset($this->message['headers']))
+			$this->message['headers'] = array();
+
+		// add header string
+		$this->message['headers'][$key] = $value;
+	}
+
+	/**
+	 * Set message subject.
+	 *
+	 * @param string $subject
+	 */
 	public function set_subject($subject) {
 		$this->subject = $subject;
 	}
 
 	/**
-	* Set variables to be replaced in subject and body.
-	*
-	* @param array $params
-	*/
+	 * Set variables to be replaced in subject and body.
+	 *
+	 * @param array $params
+	 */
 	public function set_variables($variables) {
 		$this->variables = $variables;
 	}
 
 	/**
-	* Set message body. HTML body is optional.
-	*
-	* @param string $plain_body
-	* @param string $html_body
-	*/
+	 * Set message body. HTML body is optional.
+	 *
+	 * @param string $plain_body
+	 * @param string $html_body
+	 */
 	public function set_body($plain_body, $html_body=null) {
 		$this->plain_body = $plain_body;
 		if (!is_null($html_body) && !empty($html_body))
@@ -217,16 +232,16 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 	}
 
 	/**
-	* Attach file to message. Inline attachments will have image name
-	* set as "Content-ID". Inline files can be addressed in HTML body
-	* like this:
-	*
-	* <img src="cid:example_file.png">
-	*
-	* @param string $file_name
-	* @param string $attached_name
-	* @param boolean $inline
-	*/
+	 * Attach file to message. Inline attachments will have image name
+	 * set as "Content-ID". Inline files can be addressed in HTML body
+	 * like this:
+	 *
+	 * <img src="cid:example_file.png">
+	 *
+	 * @param string $file_name
+	 * @param string $attached_name
+	 * @param boolean $inline
+	 */
 	public function attach_file($file_name, $attached_name=null, $inline=false) {
 		// encode data
 		$data = file_get_contents($file_name);
@@ -239,15 +254,15 @@ class Mandrill_Mailer extends ContactForm_Mailer {
 
 		// create structure
 		$structure = array(
-				'type'		=> $mime_type,
-				'name'		=> is_null($attached_name) ? basename($file_name) : $attached_name,
-				'content'	=> $data
-			);
+			'type'		=> $mime_type,
+			'name'		=> is_null($attached_name) ? basename($file_name) : $attached_name,
+			'content'	=> $data
+		);
 
 		// attach file
 		if ($inline)
 			$this->message['images'][] = $structure; else
-			$this->message['attachments'][] = $structure;
+				$this->message['attachments'][] = $structure;
 	}
 }
 
