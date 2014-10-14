8a990f9f9a27c4b458f5ddc8a303ed6017e3a747
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-10-15 01:47:37 +0300

    Contact form: Convert body from Markdown to HTML before sending.

--- modules/contact_form/contact_form.php
+++ modules/contact_form/contact_form.php
@@ -649,7 +649,7 @@ class contact_form extends Module {
 			foreach ($attachments as $attachment)
 				$mailer->add_attachment($attachment);
 
-			$mailer->set_body($template['plain_body'], $template['html_body']);
+			$mailer->set_body($template['plain_body'], Markdown($template['html_body']));
 			$mailer->set_variables($replacement_fields);
 
 			// send email
