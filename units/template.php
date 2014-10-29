05302427664a51f2a70f041de37cfcf7cad57fb3
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-10-29 15:40:13 +0200

    Template handler: Added `wbr` tag to list of tags without ending.

--- units/template.php
+++ units/template.php
@@ -53,7 +53,7 @@ class TemplateHandler {
 	 * List of tags that shouldn't be closed
 	 * @var array
 	 */
-	private $tags_without_end = array('br', 'hr', 'img', 'base', 'input', 'link', 'meta');
+	private $tags_without_end = array('br', 'wbr', 'hr', 'img', 'base', 'input', 'link', 'meta');
 
 	/**
 	 * If we should close all tags
