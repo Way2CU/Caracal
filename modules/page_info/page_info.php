66e035b5310d1ed67e626b0c0f4c1dc3d5b068b1
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-12-29 15:59:03 +0100

    Page info: Reverted change to allow usage of IFRAMEs.

--- modules/page_info/page_info.php
+++ modules/page_info/page_info.php
@@ -33,9 +33,6 @@ class page_info extends Module {
 		// change powered by header
 		header('X-Powered-By: Caracal/'._VERSION);
 
-		// prevent easily embedding site as iframe
-		header('X-Frame-Options: DENY');
-
 		// send encoding
 		header('Content-Type: text/html; charset=UTF-8');
 
