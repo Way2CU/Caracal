0b72b4f4b95cd5f495b685604d433b7473f8e75d
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-01-13 14:36:41 +0100

    Contact form: Add missing manager.

---
+++ modules/contact_form/units/domain_manager.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class ContactForm_DomainManager extends ItemManager {
+	private static $_instance;
+
+	/**
+	 * Constructor
+	 */
+	protected function __construct() {
+		parent::__construct('contact_form_domains');
+
+		$this->addProperty('id', 'int');
+		$this->addProperty('form', 'int');
+		$this->addProperty('domain', 'varchar');
+	}
+
+	/**
+	* Public function that creates a single instance
+	*/
+	public static function getInstance() {
+		if (!isset(self::$_instance))
+			self::$_instance = new self();
+
+		return self::$_instance;
+	}
+}
+
+?>
