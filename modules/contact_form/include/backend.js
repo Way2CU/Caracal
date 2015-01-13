7847203c9bee9bfa89082d480328ac47530c5fb8
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-01-13 14:58:00 +0100

    Contact form: Created add_domain function.

--- modules/contact_form/include/backend.js
+++ modules/contact_form/include/backend.js
@@ -5,6 +5,8 @@
  * Authors: Mladen Mijatov
  */
 
+var ContactForm = ContactForm || {};
+
 function submissions_update_result_list() {
 	var list = $('select#submission_form_list');
 	var submissions_window = window_system.getWindow('contact_form_submissions');
@@ -14,3 +16,43 @@ function submissions_update_result_list() {
 
 	submissions_window.loadContent(submissions_window.original_url + '&form=' + list.val());
 }
+
+/**
+ * Add domain to contact form domain list.
+ */
+ContactForm.add_domain = function() {
+	var list = $('div#contact_form_domain_list');
+	var domain = $('#contact_form_add input[name=domain], #contact_form_edit input[name=domain]');
+
+	// create new list item
+	var item = $('<div>');
+	var name = $('<div>');
+	var options = $('<div>');
+	var remove = $('<a>');
+
+	remove
+		.attr('href', 'javascript: ContactForm.remove_domain();')
+		.appendTo(options);
+
+	name
+		.addClass('column')
+		.css('width', 250)
+		.html(domain.val())
+		.appendTo(item);
+
+	options
+		.addClass('column')
+		.appendTo(item);
+
+	item
+		.addClass('list_item')
+		.appendTo(list);
+};
+
+/**
+ * Remove list item from domain list.
+ */
+ContactForm.remove_domain = function() {
+	var list_item = $(this).parent('.list_item');
+	list_item.remove();
+};
