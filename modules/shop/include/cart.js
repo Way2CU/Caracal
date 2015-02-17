b10047ca70d116889c5add77acf90b5497e42308
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-02-17 17:06:25 +0100

    Shop: Fixed syntax error in cart.

--- modules/shop/include/cart.js
+++ modules/shop/include/cart.js
@@ -602,7 +602,7 @@ Caracal.Shop.ItemView = function(item) {
 		self.option_remove
 				.attr('href', 'javascript: void(0);')
 				.on('click', self._handle_remove)
-				.html(language_handler.getText('shop', 'remove');
+				.html(language_handler.getText('shop', 'remove'));
 
 		// create container
 		self.container = $('<li>');
