0824a7417d4297a7cf19c050fd50abef17575f43
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-04-26 21:13:39 +0200

    Shop: Buyers manager: Added system user field.

--- modules/shop/units/shop_buyers_manager.php
+++ modules/shop/units/shop_buyers_manager.php
@@ -20,9 +20,8 @@ class ShopBuyersManager extends ItemManager {
 		$this->addProperty('first_name', 'varchar');
 		$this->addProperty('last_name', 'varchar');
 		$this->addProperty('email', 'varchar');
-		$this->addProperty('validated', 'boolean');
-		$this->addProperty('password', 'varchar');
 		$this->addProperty('guest', 'boolean');
+		$this->addProperty('system_user', 'int');
 		$this->addProperty('uid', 'varchar');
 	}
 
