9dc15d74bcdf77cca55962e10536f50118bedad5
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-04-28 20:41:16 +0200

    Docs: Version changes: Split SQL file into two.

--- docs/version_changes/0.2/01_shop_to_system_users_change.sql
+++ docs/version_changes/0.2/01_shop_to_system_users_change.sql
@@ -1,9 +1,5 @@
--- This file modifies database to adapt it to changes introduced with
--- switch in shop from local to system wide users.
+-- This file modifies database to adapt it to changes introduced with changes to
+-- `system_access` table.
 
 ALTER TABLE `system_access` ADD COLUMN `first_name` varchar(50) AFTER `fullname`;
 ALTER TABLE `system_access` ADD COLUMN `last_name` varchar(50) AFTER `first_name`;
-ALTER TABLE `shop_buyers` ADD COLUMN `system_user` int AFTER `guest`;
-ALTER TABLE `shop_buyers` DROP COLUMN `validated`;
-ALTER TABLE `shop_buyers` DROP COLUMN `password`;
-ALTER TABLE `shop_transactions` DROP COLUMN `system_user`;
---
+++ docs/version_changes/0.2/02_shop_users_change.sql
@@ -0,0 +1,7 @@
+-- This file modifies database to adapt it to changes introduced with
+-- switch in shop from local to system wide users.
+
+ALTER TABLE `shop_buyers` ADD COLUMN `system_user` int AFTER `guest`;
+ALTER TABLE `shop_buyers` DROP COLUMN `validated`;
+ALTER TABLE `shop_buyers` DROP COLUMN `password`;
+ALTER TABLE `shop_transactions` DROP COLUMN `system_user`;
