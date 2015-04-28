a5f20ab566197a63b0243e46a36b46628aba74c7
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-04-28 02:40:26 +0200

    Shop: Send email on new account.

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -2385,6 +2385,12 @@ class shop extends Module {
 							// get account object
 							$id = $manager->getInsertedID();
 							$result = $manager->getSingleItem($manager->getFieldNames(), array('id' => $id));
+
+							// send notification email
+							if (class_exists('Backend_UserManager')) {
+								$backed_user_manager = Backend_UserManager::getInstance();
+								$backed_user_manager->sendNotificationEmail($user->id);
+							}
 						}
 
 					} else if ($password == $password_confirm) {
