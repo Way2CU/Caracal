cf723eaf60020c7026c76bffe8fb863c6767dfda
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-04-29 18:20:23 +0200

    Docs: Version changes: Added agreed column to shop buyers.

--- docs/version_changes/0.2/02_shop_users_change.sql
+++ docs/version_changes/0.2/02_shop_users_change.sql
@@ -2,6 +2,7 @@
 -- switch in shop from local to system wide users.
 
 ALTER TABLE `shop_buyers` ADD COLUMN `system_user` int AFTER `guest`;
+ALTER TABLE `shop_buyers` ADD COLUMN `agreed` boolean DEFAULT '0' AFTER `system_user`;
 ALTER TABLE `shop_buyers` DROP COLUMN `validated`;
 ALTER TABLE `shop_buyers` DROP COLUMN `password`;
 ALTER TABLE `shop_transactions` DROP COLUMN `system_user`;
