8db0d83b6d3e0b2fe42f97fab6356869c05fb7ea
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-11-24 11:07:45 +0100

    Shop: Added missing property to SQL.

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -906,6 +906,7 @@ class shop extends Module {
 			`guest` boolean NOT NULL DEFAULT '0',
 			`system_user` int NULL,
 			`agreed` boolean NOT NULL DEFAULT '0',
+			`promotions` boolean NOT NULL DEFAULT '0',
 			`uid` varchar(50) NOT NULL,
 			PRIMARY KEY (`id`)
 		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=0;";
