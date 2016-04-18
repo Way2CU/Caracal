cef90138442991d8ec70078ae8aaa68db6254899
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-04-18 17:49:13 +0300

    Shop: Fixed invalid function calls.

--- modules/shop/include/checkout.js
+++ modules/shop/include/checkout.js
@@ -119,7 +119,7 @@ Caracal.Shop.BuyerInformationForm = function() {
 			var field = fields.eq(i);
 			var label = summary.find('span.' + field.attr('name'));
 
-			label.html(field.value());
+			label.html(field.val());
 		}
 	};
 
@@ -134,7 +134,7 @@ Caracal.Shop.BuyerInformationForm = function() {
 			var field = fields.eq(i);
 			var label = summary.find('span.' + field.attr('name'));
 
-			label.html(field.value());
+			label.html(field.val());
 		}
 	};
 
