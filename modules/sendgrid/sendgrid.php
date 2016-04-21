e101e8a8fbbac6133f4a334b053a0a9b5eac65bd
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-04-21 16:35:55 +0300

    Added SendGrid module.

---
+++ modules/sendgrid/sendgrid.php
@@ -0,0 +1,141 @@
+<?php
+
+/**
+ * SendGrid
+ *
+ * Integration of SendGrid email and template service into contact form. This module
+ * relies on SendGrid library being present on the system.
+ *
+ * Author: Mladen Mijatov
+ */
+use Core\Module;
+
+require_once('units/mailer.php');
+
+
+class sendgrid extends Module {
+	private static $_instance;
+
+	/**
+	 * Constructor
+	 */
+	protected function __construct() {
+		parent::__construct(__FILE__);
+
+		// register mailer
+		if (ModuleHandler::is_loaded('contact_form') {
+			$mailer = new \Modules\SendGrid\Mailer(
+					$this->language,
+					$this->settings['api_key']
+				);
+
+			$contact_form = contact_form::getInstance();
+			$contact_form->registerMailer('sendgrid', $mailer);
+		}
+
+		// register backend
+		if (ModuleHandler::is_loaded('backend') && $section == 'backend') {
+			$backend = backend::getInstance();
+
+			$mandrill_menu = new backend_MenuItem(
+					$this->getLanguageConstant('menu_sendgrid'),
+					url_GetFromFilePath($this->path.'images/icon.svg'),
+					window_Open( // on click open window
+								'sendgrid_settings',
+								370,
+								$this->getLanguageConstant('title_settings'),
+								true, true,
+								backend_UrlMake($this->name, 'settings')
+							),
+					$level=6
+				);
+
+			$backend->addMenu($this->name, $mandrill_menu);
+		}
+	}
+
+	/**
+	 * Public function that creates a single instance
+	 */
+	public static function getInstance() {
+		if (!isset(self::$_instance))
+			self::$_instance = new self();
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Transfers control to module functions
+	 *
+	 * @param array $params
+	 * @param array $children
+	 */
+	public function transferControl($params = array(), $children = array()) {
+		// global control actions
+		if (isset($params['backend_action']))
+			switch ($params['backend_action']) {
+				case 'settings':
+					$this->showSettings();
+					break;
+
+				case 'settings_save':
+					$this->saveSettings();
+					break;
+
+				default:
+					break;
+			}
+	}
+
+	/**
+	 * Event triggered upon module initialization
+	 */
+	public function onInit() {
+		$this->saveSetting('api_key', '');
+	}
+
+	/**
+	 * Event triggered upon module deinitialization
+	 */
+	public function onDisable() {
+	}
+
+	/**
+	 * Show settings form.
+	 */
+	private function showSettings() {
+		$template = new TemplateHandler('settings.xml', $this->path.'templates/');
+		$template->setMappedModule($this->name);
+
+		$params = array(
+						'form_action'	=> backend_UrlMake($this->name, 'settings_save'),
+						'cancel_action'	=> window_Close('mandrill_settings')
+					);
+
+		$template->restoreXML();
+		$template->setLocalParams($params);
+		$template->parse();
+	}
+
+	/**
+	 * Save new settings.
+	 */
+	private function saveSettings() {
+		// save setting
+		$this->saveSetting('api_key', fix_chars($_REQUEST['api_key']));
+
+		// show message
+		$template = new TemplateHandler('message.xml', $this->path.'templates/');
+		$template->setMappedModule($this->name);
+
+		$params = array(
+					'message'	=> $this->getLanguageConstant('message_saved'),
+					'button'	=> $this->getLanguageConstant('close'),
+					'action'	=> window_Close('mandrill_settings')
+				);
+
+		$template->restoreXML();
+		$template->setLocalParams($params);
+		$template->parse();
+	}
+}
---
+++ modules/sendgrid/units/mailer.php
@@ -0,0 +1,177 @@
+<?php
+
+/**
+ * SendGrid Mailer
+ *
+ * Author: Mladen Mijatov
+ */
+
+namespace Modules\SendGrid;
+
+use Core\Events;
+use \ContactForm_Mailer as ContactForm_Mailer;
+
+
+class Mailer extends ContactForm_Mailer {
+	private $language;
+	private $mailer = null;
+	private $message = null;
+	private $variables = array();
+
+	public function __construct($language, $api_key) {
+		$this->language = $language;
+		$this->mailer = new SendGrid($api_key);
+	}
+
+	/**
+	 * Get localized name.
+	 *
+	 * @return string
+	 */
+	public function get_title() {
+		return $this->language->getText('mailer_title');
+	}
+
+	/**
+	 * Prepare mailer for sending new message. This function
+	 * is ideal place to prepare to initialize internal storage
+	 * variables. No connections should be established at this
+	 * point to avoid potential timeouts.
+	 */
+	public function start_message() {
+		$this->message = new SendGrid\Email();
+	}
+
+	/**
+	 * Finalize message and send it to specified addresses.
+	 *
+	 * Note: Before sending, you *must* check if contact_form
+	 * function detectBots returns false.
+	 *
+	 * @return boolean
+	 */
+	public function send() {
+		// add variables
+		foreach ($this->variables as $key => $value)
+			$this->message->addSubstitution("%{$key}%", array($value));
+
+		// send message
+		$raw_response = $this->mailer->send($this->message);
+		$response = json_decode($raw_response);
+
+		// prepare result
+		$result = $response->message == 'success';
+
+		// trigger event
+		if ($result)
+			Events::trigger(
+				'contact_form',
+				'email-sent',
+				'sendgrid',
+				$this->message->to,
+				$this->message->subject,
+				$this->variables
+			);
+
+		return $result;
+	}
+
+	/**
+	 * Set sender of message.
+	 *
+	 * @param string $address
+	 * @param string $name
+	 */
+	public function set_sender($address, $name=null) {
+		$this->message->setFrom($address);
+		if (!is_null($name))
+			$this->message->setFromName($name);
+	}
+
+	/**
+	 * Add recipient for the message. Recipient name is optional.
+	 *
+	 * @param string $address
+	 * @param string $name
+	 */
+	public function add_recipient($address, $name=null) {
+		$this->message->addTo($address, $name);
+	}
+
+	/**
+	 * Add recipient to carbon copy (CC) field. Name is optional.
+	 *
+	 * @param string $address
+	 * @param string $name
+	 */
+	public function add_cc_recipient($address, $name=null) {
+		$this->message->addCc($address, $name);
+	}
+
+	/**
+	 * Add recipient to blind carbon copy (BCC) field. Name is optional.
+	 *
+	 * @param string $address
+	 * @param string $name
+	 */
+	public function add_bcc_recipient($address, $name=null) {
+		$this->message->addBcc($email, $name);
+	}
+
+	/**
+	 * Add custom header string.
+	 *
+	 * @param string $key
+	 * @param string $value
+	 */
+	public function add_header_string($key, $value) {
+		$this->message->addHeader($key, $value);
+	}
+
+	/**
+	 * Set message subject.
+	 *
+	 * @param string $subject
+	 */
+	public function set_subject($subject) {
+		$this->message->setSubject($subject);
+	}
+
+	/**
+	 * Set variables to be replaced in subject and body.
+	 *
+	 * @param array $params
+	 */
+	public function set_variables($variables) {
+		$this->variables = $variables;
+	}
+
+	/**
+	 * Set message body. HTML body is optional.
+	 *
+	 * @param string $plain_body
+	 * @param string $html_body
+	 */
+	public function set_body($plain_body, $html_body=null) {
+		$this->message->setText($plain_body);
+		if (!is_null($html_body) && !empty($html_body))
+			$this->message->setHtml($html_body);
+	}
+
+	/**
+	 * Attach file to message. Inline attachments will have image name
+	 * set as "Content-ID". Inline files can be addressed in HTML body
+	 * like this:
+	 *
+	 * <img src="cid:example_file.png">
+	 *
+	 * @param string $file_name
+	 * @param string $attached_name
+	 * @param boolean $inline
+	 */
+	public function attach_file($file_name, $attached_name=null, $inline=false) {
+		$this->message->addAttachment($file, $attachend_name, $inline ? $attached_name : null);
+	}
+}
+
+?>
