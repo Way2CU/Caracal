3918100eafb51017953d00a04f1163cf3591e4de
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-05-02 15:29:09 +0300

    Docs: Added change to the shop SQL.

--- docs/version_changes/0.2/02_shop_change.sql
+++ docs/version_changes/0.2/02_shop_change.sql
@@ -9,4 +9,5 @@ ALTER TABLE `shop_transactions` DROP COLUMN `system_user`;
 ALTER TABLE `shop_transactions` ADD COLUMN `payment_token` int NOT NULL DEFAULT '0' AFTER `payment_method`;
 ALTER TABLE `shop_transactions` CHANGE `token` `remote_id` varchar(255);
 ALTER TABLE `shop_delivery_address` ADD COLUMN `access_code` varchar(100) AFTER `country`;
+ALTER TABLE `shop_delivery_address` ADD COLUMN `email` varchar(127) DEFAULT '' AFTER `street2`;
 ALTER TABLE `shop_items` MODIFY `uid` varchar(64);
