bb7b2c190f64c7dc361955f67761debb857201bb
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-06-13 16:29:40 +0300

    Shop: Coupons manager: Removed discount column.

--- modules/shop/units/coupons_manager.php
+++ modules/shop/units/coupons_manager.php
@@ -24,7 +24,6 @@ class CouponsManager extends ItemManager {
 		$this->addProperty('has_timeout', 'boolean');
 		$this->addProperty('limit', 'int');
 		$this->addProperty('timeout', 'timestamp');
-		$this->addProperty('discount', 'varchar');
 	}
 
 	/**
