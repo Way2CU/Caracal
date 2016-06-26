afa1d18ad7b385683d79b58df248e03e1c25bbc9
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-06-26 16:51:23 +0300

    Updated year.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /**
  * Caracal Framework
- * Copyright (c) 2014. by Way2CU
+ * Copyright (c) 2016. by Way2CU
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
