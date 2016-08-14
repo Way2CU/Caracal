86865c6d7274ef68bd170d4579b955c4d3e536f8
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-08-14 15:12:41 +0200

    URL: Store function return value in variable.

--- units/url.php
+++ units/url.php
@@ -202,7 +202,8 @@ final class URL {
 
 		// extract values
 		preg_match(self::$matched_pattern, $query_string, $values);
-		$result = array_combine(SectionHandler::get_matched_params()[1], $values[0]);
+		$params = SectionHandler::get_matched_params();
+		$result = array_combine($params[1], $values[0]);
 
 		// modify global variables
 		switch ($_SERVER['REQUEST_METHOD']) {
