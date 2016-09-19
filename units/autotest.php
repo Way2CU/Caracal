4bc1066f4c03ede3176f0f3d9d90c7222fe1c198
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-09-19 17:26:46 +0200

    Changed function name.

--- units/autotest.php
+++ units/autotest.php
@@ -21,7 +21,7 @@ final class AutoTest {
 	/**
 	 * Public function that creates a single instance
 	 */
-	public static function getInstance() {
+	public static function get_instance() {
 		if (!isset(self::$_instance))
 			self::$_instance = new self();
 
