8dd4d53aca8a438d0f537ab2918a7e203360e897
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-10-24 18:56:21 +0200

    Shop: Added item remarks manager.

---
+++ modules/shop/units/item_remarks_manager.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Remarks manager for shop items.
+ *
+ * Author: Mladen Mijatov
+ */
+
+namespace Modules\Shop\Item;
+use ItemManager;
+
+
+class RemarkManager extends ItemManager {
+	private static $_instance;
+
+	/**
+	 * Constructor
+	 */
+	protected function __construct() {
+		parent::__construct('shop_item_remarks');
+
+		$this->addProperty('id', 'int');
+		$this->addProperty('item', 'int');
+		$this->addProperty('remark', 'text');
+	}
+
+	/**
+	* Public function that creates a single instance
+	*/
+	public static function getInstance() {
+		if (!isset(self::$_instance))
+			self::$_instance = new self();
+
+		return self::$_instance;
+	}
+}
+?>
