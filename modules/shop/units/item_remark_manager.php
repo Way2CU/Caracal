fee1532ed323b8236c4493f99f2b349549550934
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-10-24 19:23:40 +0200

    Fixed issues with old style `getInstance` call.

--- modules/shop/units/item_remark_manager.php
+++ modules/shop/units/item_remark_manager.php
@@ -27,7 +27,7 @@ class RemarkManager extends ItemManager {
 	/**
 	* Public function that creates a single instance
 	*/
-	public static function getInstance() {
+	public static function get_instance() {
 		if (!isset(self::$_instance))
 			self::$_instance = new self();
 
