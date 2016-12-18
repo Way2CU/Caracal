683a4194a19798fff08e350b416a1666535419ad
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-12-18 18:30:33 +0100

    Cache: Fix variable name.
    Used `is_caching` instead of `should_cache`. This caused cache to be
    indefinitely generated.

--- units/cache/cache.php
+++ units/cache/cache.php
@@ -122,7 +122,7 @@ class Manager {
 	public function is_cached() {
 		$result = false;
 
-		if (!is_null($this->provider) && $this->is_caching)
+		if (!is_null($this->provider) && $this->should_cache)
 			$result = $this->provider->is_cached($this->uid);
 
 		return $result;
