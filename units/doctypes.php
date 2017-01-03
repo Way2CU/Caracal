d683be7e4f33ec475351d1daee08a217a0eb08aa
Mladen Mijatov <meaneye.rcf@gmail.com>
2017-01-03 18:38:09 +0100

    Removed invalid doctype.

--- units/doctypes.php
+++ units/doctypes.php
@@ -8,8 +8,7 @@
 $document_types = array(
 	'html4.01'   => '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">',
 	'html5'      => '<!DOCTYPE html>',
-	'xml1.0'     => '<?xml version="1.0" encoding="UTF-8"?>',
-	'sitemap0.9' => '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+	'xml1.0'     => '<?xml version="1.0" encoding="UTF-8"?>'
 );
 
 ?>
