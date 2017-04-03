6d4f1bce162392505a905674ababeef1be0a717c
Mladen Mijatov <meaneye.rcf@gmail.com>
2017-04-03 14:35:15 +0300

    Shop: Add menu item for suppliers.

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -332,6 +332,19 @@ class shop extends Module {
 				5  // level
 			));
 
+			$shop_menu->addChild(null, new backend_MenuItem(
+				$this->get_language_constant('menu_suppliers'),
+				URL::from_file_path($this->path.'images/suppliers.svg'),
+				window_Open( // on click open window
+					'shop_suppliers',
+					400,
+					$this->get_language_constant('title_suppliers'),
+					true, true,
+					backend_UrlMake($this->name, 'suppliers')
+				),
+				5  // level
+			));
+
 			// delivery methods menu
 			$delivery_menu = new backend_MenuItem(
 				$this->get_language_constant('menu_delivery_methods'),
