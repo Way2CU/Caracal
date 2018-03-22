1c3f00e2ffe173f92152db573acf9b4956928448
Mladen Mijatov <meaneye.rcf@gmail.com>
2018-03-22 11:30:28 +0200

    Kodkod: Initial version of module.
    Only login mechanism has been built.

---
+++ modules/kodkod/kodkod.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Kodkod Integration
+ *
+ * This module is used as adapter for communication with Kodkod service. It provides
+ * various third party integrations as well as authentication mechanism.
+ */
+use Core\Module;
+use Core\Session\Manager as Session;
+use Modules\Kodkod\Mechanism;
+
+
+class kodkod extends Module {
+	private static $_instance;
+	private $login_mechanism;
+
+	/**
+	 * Constructor
+	 */
+	protected function __construct() {
+		parent::__construct(__FILE__);
+
+		// create login mechanism
+		$this->login_mechanism = new Mechanism();
+		Session::register_login_mechanism('kodkod', $this->login_mechanism);
+	}
+
+	/**
+	 * Public function that creates a single instance
+	 */
+	public static function get_instance() {
+		if (!isset(self::$_instance))
+			self::$_instance = new self();
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Transfers control to module functions
+	 *
+	 * @param array $params
+	 * @param array $children
+	 */
+	public function transfer_control($params = array(), $children = array()) {
+	}
+
+	/**
+	 * Event triggered upon module initialization
+	 */
+	public function initialize() {
+	}
+
+	/**
+	 * Event triggered upon module deinitialization
+	 */
+	public function cleanup() {
+	}
+}
+
+?>
---
+++ modules/kodkod/units/login_mechanism.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Kodkod Authentication Mechanism
+ *
+ * This mechanism relies on Kodkod storage service running on the same server
+ * as Caracal. Users are logged in after credentials are verified with storage
+ * service.
+ */
+namespace Modules\Kodkod;
+
+
+class Mechanism extends Core\Session\Mechanism {
+	const ENDPOINT = 'http://localhost';
+
+	/**
+	 * Perform authentication and return boolean value denoting
+	 * success of the action.
+	 *
+	 * @return mixed
+	 */
+	public function login($params=null) {
+		// prepare data for sending
+		$result = null;
+		$data = array(
+				'id'      => fix_id($_REQUEST['id']),
+				'address' => fix_chars($_SERVER['REMOTE_ADDR']),
+				'token'   => fix_chars($_REQUEST['token'])
+			);
+
+		// get data from storage service
+		$query = http_build_query($data);
+		$raw_response = file_get_contents(self::ENDPOINT.'?'.$query);
+
+		if ($raw_response !== NULL) {
+			$response = json_decode($raw_response);
+			if ($response !== NULL && $response->success)
+				$result = array();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Perform logout operation and return boolean value denoting
+	 * success of the action.
+	 *
+	 * @return boolean
+	 */
+	public function logout() {
+		return true;  // we always allow logging out as we don't have to do any cleanup
+	}
+
+	/**
+	 * Return associative array containing data for session variables.
+	 * This function is called immediately after successful login.
+	 *
+	 * Example return structure:
+	 *
+	 * @param array $data
+	 * @return array
+	 */
+	public function get_data($data) {
+		return array(
+				'uid'       => 0,
+				'level'     => 10,
+				'username'  => 'kodkod',
+				'fist_name' => 'Web',
+				'last_name' => 'Application'
+			);
+	}
+}
+
+
+?>
