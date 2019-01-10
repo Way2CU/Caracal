be24d2237906375c3bb8c3dde87d3aeed37841b8
Mladen Mijatov <meaneye.rcf@gmail.com>
2019-01-10 15:28:49 +0100

    Gallery: Backend script: Rewrote to properly update URL for the window.

--- modules/gallery/include/backend.js
+++ modules/gallery/include/backend.js
@@ -5,11 +5,26 @@ var Caracal = Caracal || {};
 
 
 function gallery_update_image_list() {
-	var list = $('select#gallery_images_group');
 	var gallery_window = Caracal.window_system.get_window('gallery_images');
+	var list = gallery_window.ui.container.querySelector('select#gallery_images_group');
+	var raw_url = gallery_window.url;
 
-	if (gallery_window.original_url == undefined)
-		gallery_window.original_url = gallery_window.url;
+	// make sure we have URL to operate on
+	if (raw_url === undefined)
+		raw_url = window.location.toString();
 
-	gallery_window.load_content(gallery_window.original_url + '&group=' + list.val());
+	// create params object for easier modification
+	var url = new URL(raw_url);
+	var params = new URLSearchParams(url.search);
+
+	// enclose parameter is needed only initially
+	if (params.has('enclose'))
+		params.delete('enclose');
+
+	// modify selected group
+	params.set('group', list.value())
+	url.search = params.toString();
+
+	// reload window content
+	gallery_window.load_content(url.toString());
 }
