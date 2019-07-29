445dc51552754994bdabde46d0b1cb682d05425d
Mladen Mijatov <meaneye.rcf@gmail.com>
2019-07-29 14:54:48 +0200

    Docs: Fixed version change SQL file.

--- docs/version_changes/0.4/contact_form.sql
+++ docs/version_changes/0.4/contact_form.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `contact_form_fields` ADD COLUMN `checked` boolean NOT NULL DEFAULT '0' AFTER `colors`;
+ALTER TABLE `contact_form_fields` ADD COLUMN `checked` boolean NOT NULL DEFAULT '0' AFTER `required`;
