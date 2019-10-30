34a69c8e0627ff87d10792af0f78681590e4f4f7
Mladen Mijatov <meaneye.rcf@gmail.com>
2019-10-30 13:15:10 +0100

    News: Fixed invalid class name.

--- modules/news/news.php
+++ modules/news/news.php
@@ -338,7 +338,7 @@ class news extends Module {
 			return array();
 
 		// initialize managers and data
-		$manager = ItemManager::get_instance();
+		$manager = NewsManager::get_instance();
 		$result = array();
 		$conditions = array(
 				'visible' => 1,
