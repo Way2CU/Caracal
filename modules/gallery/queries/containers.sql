6fdb688b7dfe88e13201a2b3c2ab47efbdc37517
Mladen Mijatov <meaneye.rcf@gmail.com>
2023-10-09 15:05:15 +0200

    Gallery: Queries: Specify text_id size.

--- modules/gallery/queries/containers.sql
+++ modules/gallery/queries/containers.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS `gallery_containers` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`text_id` varchar COLLATE utf8_bin NULL,
+	`text_id` varchar(32) COLLATE utf8_bin NULL,
 	`name` ml_varchar(50) NOT NULL,
 	`description` ml_text NOT NULL DEFAULT '',
 	PRIMARY KEY (`id`)
